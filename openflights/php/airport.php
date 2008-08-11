3098057fa0f55545b410b2624c188470a64e885f
jpatokal <jpatokal@a57d767e-1c7c-476a-a379-f7d483c1f09d>
2008-08-11 16:53:25 +0000

    Content-sensitive help and misc tweaks

--- openflights/php/airport.php
+++ openflights/php/airport.php
@@ -15,7 +15,7 @@ if(!$id) {
 }
 
 // List of all flights originating from an airport
-$sql = "SELECT s.iata AS src_iata,s.apid AS src_apid,d.iata AS dst_iata,d.apid AS dst_apid,f.code,DATE(f.src_time) as src_date,distance,duration,seat,seat_type,class,reason FROM flights AS f,airports AS s,airports AS d WHERE f.uid=" . $uid . " AND f.src_apid=s.apid AND f.dst_apid=d.apid AND (s.apid=" . $id . " OR d.apid=" . $id . ")";
+$sql = "SELECT s.iata AS src_iata,s.apid AS src_apid,d.iata AS dst_iata,d.apid AS dst_apid,f.code,DATE(f.src_time) as src_date,distance,DATE_FORMAT(duration, '%H:%i') AS duration,seat,seat_type,class,reason,fid FROM flights AS f,airports AS s,airports AS d WHERE f.uid=" . $uid . " AND f.src_apid=s.apid AND f.dst_apid=d.apid AND (s.apid=" . $id . " OR d.apid=" . $id . ")";
 $result = mysql_query($sql, $db);
 $first = true;
 while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
@@ -24,6 +24,6 @@ while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
   } else {
     printf("\t");
   }  
-  printf ("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s", $row["src_iata"], $row["src_apid"], $row["dst_iata"], $row["dst_apid"], $row["code"], $row["src_date"], $row["distance"], $row["duration"], $row["seat"], $row["seat_type"], $row["class"], $row["reason"]);
+  printf ("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s", $row["src_iata"], $row["src_apid"], $row["dst_iata"], $row["dst_apid"], $row["code"], $row["src_date"], $row["distance"], $row["duration"], $row["seat"], $row["seat_type"], $row["class"], $row["reason"], $row["fid"]);
 }
 ?>
