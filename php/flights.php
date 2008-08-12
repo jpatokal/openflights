be2ec079407b6b6b7318129385be32627b35ca5a
Jani Patokallio <jpatokal@users.noreply.github.com>
2008-08-12 10:39:38 +0000

    Add "List flights" button Add plane registrations Flight view now shows plane type Cosmetic tweaks

---
+++ php/flights.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+$uid = $_SESSION["uid"];
+if(!$uid or empty($uid)) {
+  // If not logged in, default to demo mode
+  $uid = 1;
+}
+
+$db = mysql_connect("localhost", "openflights");
+mysql_select_db("flightdb",$db);
+
+$apid = $HTTP_POST_VARS["id"];
+if(!$apid) {
+  // For easier debugging
+  $apid = $HTTP_GET_VARS["id"];
+}
+
+$trid = $HTTP_POST_VARS["trid"];
+$alid = $HTTP_POST_VARS["alid"];
+
+// List of all this user's flights
+$sql = "SELECT s.iata AS src_iata,s.apid AS src_apid,d.iata AS dst_iata,d.apid AS dst_apid,f.code,DATE(f.src_time) as src_date,distance,DATE_FORMAT(duration, '%H:%i') AS duration,seat,seat_type,class,reason,p.name,registration,fid FROM airports AS s,airports AS d, flights AS f LEFT JOIN planes AS p ON f.plid=p.plid WHERE f.uid=" . $uid . " AND f.src_apid=s.apid AND f.dst_apid=d.apid";
+
+// ...filtered by airport (optional)
+if($apid && $apid != 0) {
+  $sql = $sql . " AND (s.apid=" . $apid . " OR d.apid=" . $apid . ")";
+}
+
+// Add filters, if any
+if($trid && $trid != "0") {
+  $sql = $sql . " AND trid= " . $trid;
+}
+if($alid && $alid != "0") {
+  $sql = $sql . " AND alid= " . $alid;
+}
+
+// Execute!
+$result = mysql_query($sql, $db);
+$first = true;
+while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+  if($first) {
+    $first = false;
+  } else {
+    printf("\t");
+  }  
+  printf ("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s", $row["src_iata"], $row["src_apid"], $row["dst_iata"], $row["dst_apid"], $row["code"], $row["src_date"], $row["distance"], $row["duration"], $row["seat"], $row["seat_type"], $row["class"], $row["reason"], $row["fid"], $row["name"], $row["registration"]);
+}
+?>
--- php/submit.php
+++ php/submit.php
@@ -16,6 +16,7 @@ $seat = $HTTP_POST_VARS["seat"];
 $seat_type = $HTTP_POST_VARS["type"];
 $class = $HTTP_POST_VARS["class"];
 $reason = $HTTP_POST_VARS["reason"];
+$registration = $HTTP_POST_VARS["registration"];
 $plid = $HTTP_POST_VARS["plid"];
 $alid = $HTTP_POST_VARS["alid"];
 $trid = $HTTP_POST_VARS["trid"];
@@ -23,9 +24,9 @@ $trid = $HTTP_POST_VARS["trid"];
 $db = mysql_connect("localhost", "openflights");
 mysql_select_db("flightdb",$db);
 
-$sql = sprintf("INSERT INTO flights(uid, src_apid, src_time, dst_apid, duration, distance, code, seat, seat_type, class, reason, plid, alid, trid) VALUES (%s, %s, '%s', %s, '%s', %s, '%s', '%s', '%s', '%s', '%s', %s, %s, %s)",
-       $uid, $src_apid, $src_date, $dst_apid, $duration, $distance, $number, $seat, $seat_type, $class, $reason, $plid, $alid, $trid);
-print $sql;
+$sql = sprintf("INSERT INTO flights(uid, src_apid, src_time, dst_apid, duration, distance, registration, code, seat, seat_type, class, reason, plid, alid, trid) VALUES (%s, %s, '%s', %s, '%s', %s, '%s', '%s', '%s', '%s', '%s', '%s', %s, %s, %s)",
+	       $uid, $src_apid, $src_date, $dst_apid, $duration, $distance, $registration, $number, $seat, $seat_type, $class, $reason, $plid, $alid, $trid);
 
-mysql_query($sql, $db) or die ('Adding plane to DB failed.');
+mysql_query($sql, $db) or die ('0;Adding plane to DB failed: ' . $sql);
+printf("1;Flight added.");
 ?>
