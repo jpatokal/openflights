97a6f9ee8383687b9c511175a30f82bfcc6824bd
jpatokal <jpatokal@a57d767e-1c7c-476a-a379-f7d483c1f09d>
2008-09-07 14:27:10 +0000

    Log output

--- openflights/sql/update-demo.sql
+++ openflights/sql/update-demo.sql
@@ -3,3 +3,5 @@
 --
 delete from flights where uid=1;
 insert into flights(uid,src_apid,src_time,dst_apid,distance,code,seat,seat_type,class,reason,plid,alid,trid,duration,registration,note,upd_time) select 1,src_apid,src_time,dst_apid,distance,code,seat,seat_type,class,reason,plid,alid,trid,duration,registration,note,upd_time from flights where date_sub(now(), interval 7 day) < upd_time;
+select now(), count(*) from flights where uid=1;
+
