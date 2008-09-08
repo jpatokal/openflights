0f280f4a1332962bf83df41f6e01e75c02003187
Jani Patokallio <jpatokal@users.noreply.github.com>
2008-09-08 12:23:51 +0000

    trid to NULL

--- sql/update-demo.sql
+++ sql/update-demo.sql
@@ -2,6 +2,7 @@
 -- Refresh the "demo" flights shown when logging in
 --
 delete from flights where uid=1;
-insert into flights(uid,src_apid,src_time,dst_apid,distance,code,seat,seat_type,class,reason,plid,alid,trid,duration,registration,note,upd_time) select 1,src_apid,src_time,dst_apid,distance,code,seat,seat_type,class,reason,plid,alid,trid,duration,registration,note,upd_time from flights where date_sub(now(), interval 7 day) < upd_time;
+-- Note: uid forced to 0 (demo), trid forced to NULL (not in trip)
+insert into flights(uid,src_apid,src_time,dst_apid,distance,code,seat,seat_type,class,reason,plid,alid,trid,duration,registration,note,upd_time) select 1,src_apid,src_time,dst_apid,distance,code,seat,seat_type,class,reason,plid,alid,null,duration,registration,note,upd_time from flights where date_sub(now(), interval 7 day) < upd_time;
 select now(), count(*) from flights where uid=1;
 
