ddb9e5c2a4456abf50f285b44c180deaf1c61f04
Jani Patokallio <jpatokal@users.noreply.github.com>
2008-09-11 13:34:51 +0000

    Adding new airports now implemented

--- php/apsearch.php
+++ php/apsearch.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include 'helper.php';
 
 $airport = $HTTP_POST_VARS["airport"];
@@ -7,12 +8,47 @@ $icao = $HTTP_POST_VARS["icao"];
 $city = $HTTP_POST_VARS["city"];
 $country = $HTTP_POST_VARS["country"];
 $code = $HTTP_POST_VARS["code"];
+$myX = $HTTP_POST_VARS["x"];
+$myY = $HTTP_POST_VARS["y"];
+$elevation = $HTTP_POST_VARS["elevation"];
 $dbname = $HTTP_POST_VARS["db"];
 $iatafilter = $HTTP_POST_VARS["iatafilter"];
 $offset = $HTTP_POST_VARS["offset"];
+$action = $HTTP_POST_VARS["action"];
 
 $db = mysql_connect("localhost", "openflights");
 mysql_select_db("flightdb",$db);
+
+if($action == "RECORD") {
+  $uid = $_SESSION["uid"];
+  if(!$uid or empty($uid)) {
+    printf("0;Your session has timed out, please log in again.");
+    exit;
+  }
+
+  // Check for duplicates
+  $sql = "SELECT * FROM airports WHERE icao='" . mysql_real_escape_string($icao) . "'";
+  $result = mysql_query($sql, $db);
+  if($row = mysql_fetch_array($result, MYSQL_NUM)) {
+    printf("0;An airport using the ICAO code " . $icao . " exists already.");
+    exit;
+  }
+
+  $sql = sprintf("INSERT INTO airports(name,city,country,iata,icao,x,y,elevation,uid) VALUES('%s', '%s', '%s', '%s', '%s', %s, %s, %s, %s)",
+		 mysql_real_escape_string($airport), 
+		 mysql_real_escape_string($city),
+		 mysql_real_escape_string($country),
+		 mysql_real_escape_string($iata),
+		 mysql_real_escape_string($icao),
+		 mysql_real_escape_string($myX),
+		 mysql_real_escape_string($myY),
+		 mysql_real_escape_string($elevation),
+		 $uid);
+
+  mysql_query($sql, $db) or die('0;Adding new airport failed' . $sql);
+  printf('1;' . mysql_insert_id() . ';New airport successfully added.');
+  exit;
+}
 
 if(! $dbname) {
   $dbname = "airports";
@@ -63,7 +99,7 @@ if($row = mysql_fetch_array($result2, MYSQL_NUM)) {
 printf("%s;%s;%s", $offset, $max, $sql);
 
 while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-  printf ("\n%s:%s:%s:%s;%s", $row["iata"], $row["apid"], $row["x"], $row["y"], format_airport($row));
+  printf ("\n%s;%s;%s;%s;%s;%s;%s;%s;%s", $row["iata"], $row["icao"], $row["apid"], $row["x"], $row["y"], $row["elevation"], $row["name"], $row["code"], format_airport($row));
 }
 
 ?>
