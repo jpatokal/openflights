894cdb85f5ccf2e97e4d524e27fa2a901ec0b68e
Jani Patokallio <jpatokal@users.noreply.github.com>
2008-09-13 05:28:31 +0000

    Limit to 500 newest in last three days

--- sql/update-demo.sql
+++ sql/update-demo.sql
@@ -3,6 +3,5 @@
 --
 delete from flights where uid=1;
 -- Note: uid forced to 0 (demo), trid forced to NULL (not in trip)
-insert into flights(uid,src_apid,src_time,dst_apid,distance,code,seat,seat_type,class,reason,plid,alid,trid,duration,registration,note,upd_time) select 1,src_apid,src_time,dst_apid,distance,code,seat,seat_type,class,reason,plid,alid,null,duration,registration,note,upd_time from flights where date_sub(now(), interval 7 day) < upd_time;
+insert into flights(uid,src_apid,src_time,dst_apid,distance,code,seat,seat_type,class,reason,plid,alid,trid,duration,registration,note,upd_time) select 1,src_apid,src_time,dst_apid,distance,code,seat,seat_type,class,reason,plid,alid,null,duration,registration,note,upd_time from flights where date_sub(now(), interval 3 day) < upd_time order by upd_time desc limit 500;
 select now(), count(*) from flights where uid=1;
-
