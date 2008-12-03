b9f32abc4b401dd07789f70b6c66068b513da751
Jani Patokallio <jpatokal@users.noreply.github.com>
2008-12-03 10:52:09 +0000

    Select every 5th flight

--- sql/update-demo.sql
+++ sql/update-demo.sql
@@ -2,6 +2,7 @@
 -- Refresh the "demo" flights shown when logging in
 --
 delete from flights where uid=1;
+-- Select every fifth flight
 -- Note: uid forced to 0 (demo), trid forced to NULL (not in trip)
-insert into flights(uid,src_apid,src_time,dst_apid,distance,code,seat,seat_type,class,reason,plid,alid,trid,duration,registration,note,upd_time,opp) select 1,src_apid,src_time,dst_apid,distance,code,seat,seat_type,class,reason,plid,alid,null,duration,registration,note,upd_time,opp from flights where date_sub(now(), interval 3 day) < upd_time order by upd_time desc limit 200;
+insert into flights(uid,src_apid,src_time,dst_apid,distance,code,seat,seat_type,class,reason,plid,alid,trid,duration,registration,note,upd_time,opp) select 1,src_apid,src_time,dst_apid,distance,code,seat,seat_type,class,reason,plid,alid,null,duration,registration,note,upd_time,opp from flights where fid % 5 = 0 and date_sub(now(), interval 3 day) < upd_time order by upd_time desc limit 200;
 select now(), count(*) from flights where uid=1;
