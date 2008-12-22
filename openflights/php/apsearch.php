0f847b43a3be042515401184b63ebfa29a31ac39
jpatokal <jpatokal@a57d767e-1c7c-476a-a379-f7d483c1f09d>
2008-12-22 13:48:06 +0000

    * Ability to edit your own airports added. * GAD airports can now be loaded for airport entry.

--- openflights/php/apsearch.php
+++ openflights/php/apsearch.php
@@ -17,40 +17,62 @@ $dbname = $HTTP_POST_VARS["db"];
 $iatafilter = $HTTP_POST_VARS["iatafilter"];
 $offset = $HTTP_POST_VARS["offset"];
 $action = $HTTP_POST_VARS["action"];
+$apid = $HTTP_POST_VARS["apid"];
 
 $db = mysql_connect("localhost", "openflights");
 mysql_select_db("flightdb",$db);
 
+$uid = $_SESSION["uid"];
+
 if($action == "RECORD") {
-  $uid = $_SESSION["uid"];
   if(!$uid or empty($uid)) {
     printf("0;Your session has timed out, please log in again.");
     exit;
   }
 
-  // Check for duplicates
-  if($icao != "") {
-    $sql = "SELECT * FROM airports WHERE icao='" . mysql_real_escape_string($icao) . "'";
-    $result = mysql_query($sql, $db);
-    if($row = mysql_fetch_array($result, MYSQL_NUM)) {
-      printf("0;An airport using the ICAO code " . $icao . " exists already.");
-      exit;
+  if(! $apid || $apid == "") {
+    // Adding a new airport, so first check for duplicates
+    if($icao != "") {
+      $sql = "SELECT * FROM airports WHERE icao='" . mysql_real_escape_string($icao) . "'";
+      $result = mysql_query($sql, $db);
+      if($row = mysql_fetch_array($result, MYSQL_NUM)) {
+	printf("0;Sorry, an airport using the ICAO code " . $icao . " exists already.  Please double-check.");
+	exit;
+      }
     }
+    
+    $sql = sprintf("INSERT INTO airports(name,city,country,iata,icao,x,y,elevation,uid) VALUES('%s', '%s', '%s', '%s', %s, %s, %s, %s, %s)",
+		   mysql_real_escape_string($airport), 
+		   mysql_real_escape_string($city),
+		   mysql_real_escape_string($country),
+		   mysql_real_escape_string($iata),
+		   $icao == "" ? "NULL" : "'" . mysql_real_escape_string($icao) . "'",
+		   mysql_real_escape_string($myX),
+		   mysql_real_escape_string($myY),
+		   mysql_real_escape_string($elevation),
+		   $uid);
+  } else {
+    // Editing an existing airport
+    // ##TODO## un-hardcode admin write access
+    $sql = sprintf("UPDATE airports SET name='%s', city='%s', country='%s', iata='%s', icao=%s, x=%s, y=%s, elevation=%s WHERE apid=%s AND (uid=%s OR uid=3)",
+		   mysql_real_escape_string($airport), 
+		   mysql_real_escape_string($city),
+		   mysql_real_escape_string($country),
+		   mysql_real_escape_string($iata),
+		   $icao == "" ? "NULL" : "'" . mysql_real_escape_string($icao) . "'",
+		   mysql_real_escape_string($myX),
+		   mysql_real_escape_string($myY),
+		   mysql_real_escape_string($elevation),
+		   mysql_real_escape_string($apid),
+		   mysql_real_escape_string($uid));
   }
 
-  $sql = sprintf("INSERT INTO airports(name,city,country,iata,icao,x,y,elevation,uid) VALUES('%s', '%s', '%s', '%s', %s, %s, %s, %s, %s)",
-		 mysql_real_escape_string($airport), 
-		 mysql_real_escape_string($city),
-		 mysql_real_escape_string($country),
-		 mysql_real_escape_string($iata),
-		 $icao == "" ? "NULL" : "'" . mysql_real_escape_string($icao) . "'",
-		 mysql_real_escape_string($myX),
-		 mysql_real_escape_string($myY),
-		 mysql_real_escape_string($elevation),
-		 $uid);
-
   mysql_query($sql, $db) or die('0;Adding new airport failed' . $sql);
-  printf('1;' . mysql_insert_id() . ';New airport successfully added.');
+  if(! $apid || $apid == "") {
+    printf('1;' . mysql_insert_id() . ';New airport successfully added.');
+  } else {
+    printf('1;' . $apid . ';Airport successfully edited.');
+  }
   exit;
 }
 
@@ -103,7 +125,23 @@ if($row = mysql_fetch_array($result2, MYSQL_NUM)) {
 printf("%s;%s;%s", $offset, $max, $sql);
 
 while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-  printf ("\n%s;%s;%s;%s;%s;%s;%s;%s;%s;%s", $row["iata"], $row["icao"], $row["apid"], $row["x"], $row["y"], $row["elevation"], $row["name"], $row["code"], format_airport($row), $row["uid"]);
+  if($dbname == "airports_dafif") {
+    $country = $row["code"];
+  } else {
+    $country = $row["country"];
+  }
+ 
+  // ##TODO## admin flag instead of hardcoding...
+  if($row["uid"] || $uid == "3" ) {
+    if($row["uid"] == $uid || $uid == "3") {
+      $ap_uid = "own"; // editable
+    } else {
+      $ap_uid = "user"; // added by another user
+    }
+  } else {
+    $ap_uid = null; // in DB
+  }
+  printf ("\n%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s", $row["iata"], $row["icao"], $row["apid"], $row["x"], $row["y"], $row["elevation"], $row["name"], $country, format_airport($row), $ap_uid, $row["city"]);
 }
 
 ?>
