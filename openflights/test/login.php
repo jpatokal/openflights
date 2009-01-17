b94412e3bbb8b70915deda618df4b249e8da8074
jpatokal <jpatokal@a57d767e-1c7c-476a-a379-f7d483c1f09d>
2009-01-17 09:35:00 +0000

    A few missing test cases

--- openflights/test/login.php
+++ openflights/test/login.php
@@ -9,16 +9,41 @@ include_once(dirname(__FILE__) . '/config.php');
 
 // Standard log in
 class SuccessfulLoginTest extends WebTestCase {
-  function testHomepage() {
+  function test() {
     global $webroot, $settings;
     login($this);
     $this->assertText('1;');
   }
 }
 
+// Legacy login test (where username hash was built using uppercase chars)
+class LegacyLoginTest extends WebTestCase {
+  function test() {
+    global $webroot, $settings;
+
+    $username = "LegacyUser";
+    $password = "foobar";
+    $hash = md5($password . $username);
+
+    $db = db_connect();
+    $sql = "INSERT INTO users(name,password) VALUES('$username','$hash')";
+    $result = mysql_query($sql, $db);
+    $this->assertTrue(mysql_affected_rows() == 1, "Legacy user added");
+
+    login($this, $username, $password);
+    $this->assertText('1;');
+
+    $sql = "DELETE FROM users WHERE name='$username'";
+    $result = mysql_query($sql, $db);
+    $this->assertTrue(mysql_affected_rows() == 1, "Legacy user deleted");
+  }
+}
+
+    $db = db_connect();
+
 // Wrong password
 class WrongPasswordLoginTest extends WebTestCase {
-  function testHomepage() {
+  function test() {
     global $webroot, $settings;
     login($this, $settings["username"], "incorrect");
     $this->assertText('0;');
