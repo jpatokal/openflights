215c19a0ac829857d18a64c9f342544095fa2744
Jani Patokallio <jpatokal@users.noreply.github.com>
2009-01-19 11:25:52 +0000

    Basic OF data

--- facebook/index.php
+++ facebook/index.php
@@ -16,12 +16,16 @@ $user_id = $facebook->require_login();
 // Greet the currently logged-in user!
 echo "<p>Hello, <fb:name uid=\"$user_id\" useyou=\"false\" />!</p>";
 
-// Print out at most 25 of the logged-in user's friends,
-// using the friends.get API method
-echo "<p>Friends:";
-$friends = $facebook->api_client->friends_get();
-$friends = array_slice($friends, 0, 25);
-foreach ($friends as $friend) {
-  echo "<br>$friend";
+// Statistics
+// Number of flights, total distance (mi), total duration (minutes), public/open
+$db = mysql_connect("localhost", "openflights");
+mysql_select_db("flightdb",$db);
+$sql = "SELECT COUNT(*) AS count, SUM(distance) AS distance, SUM(TIME_TO_SEC(duration))/60 AS duration FROM flights AS f WHERE uid=3";
+$result = mysql_query($sql, $db);
+if($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+  if($row["count"] == "0" && $user && $user != "0") {
+    die('Error;This user has no flights.');
+  }
+  printf("<p>%s;%s;%s;%s;%s;%s;%s;%s</p>", $row["count"], $row["distance"], $row["duration"], $public, $elite,
+	 $logged_in, $editor, $challenge);
 }
-echo "</p>";
