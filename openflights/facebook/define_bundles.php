ddf569914e3cf09383e274c75f72c123c4555577
jpatokal <jpatokal@a57d767e-1c7c-476a-a379-f7d483c1f09d>
2009-01-20 06:18:21 +0000

    More bug fixing

--- openflights/facebook/define_bundles.php
+++ openflights/facebook/define_bundles.php
@@ -8,15 +8,15 @@ $facebook = new Facebook($appapikey, $appsecret);
 $facebook->api_client->session_key = $infinitesessionkey;
 
 $one_line_story_templates = array();
-$one_line_story_templates[] = '{*actor*} added <b>{*count*}</b> new flights, including a flight from <b>{*src*}</b> to <b>{*dst*}</b>, to <fb:pronoun uid=\"actor\" useyou=\"false\" possessive=\"true\"/> <a href="http://openflights.org/user/{*ofname*}">OpenFlights</a>!';
-$one_line_story_templates[] = '{*actor*} added a flight from <b>{*src*}</b> to <b>{*dst*}</b> to <fb:pronoun uid=\"actor\" useyou=\"false\" possessive=\"true\"/> <a href="http://openflights.org/user/{*ofname*}">OpenFlights</a>!';
+$one_line_story_templates[] = '{*actor*} added <b>{*count*}</b> new flights, including a flight from <b>{*src*}</b> to <b>{*dst*}</b>, to <fb:pronoun uid="actor" useyou="false" possessive="true"/> <a href="http://openflights.org/user/{*ofname*}">OpenFlights</a>!';
+$one_line_story_templates[] = '{*actor*} added a flight from <b>{*src*}</b> to <b>{*dst*}</b> to <fb:pronoun uid="actor" useyou="false" possessive="true"/> <a href="http://openflights.org/user/{*ofname*}">OpenFlights</a>!';
 
 $short_story_templates = array();
-$short_story_templates[] = array('template_title' => '{*actor*} added <b>{*count*}</b> new flights to <fb:pronoun uid=\"actor\" useyou=\"false\" possessive=\"true\"/> <a href="http://openflights.org/user/{*ofname*}">OpenFlights</a>!',
-				 'template_body' => 'Including the flight from <b>{*src*}</b> to <b>{*dst*}</b>, the flights cover a distance of <b>{*distance*}</b> miles.');
+$short_story_templates[] = array('template_title' => '{*actor*} added <b>{*count*}</b> new flights to <fb:pronoun uid="actor" useyou="false" possessive="true"/> <a href="http://openflights.org/user/{*ofname*}">OpenFlights</a>!',
+				 'template_body' => 'Including the flight from <b>{*src*}</b> to <b>{*dst*}</b>, the flights span a distance of <b>{*distance*}</b> miles.');
 
-$full_story_template = array('template_title' => '{*actor*} added <b>{*count*}</b> new flights to <fb:pronoun uid=\"actor\" useyou=\"false\" possessive=\"true\"/> <a href="http://openflights.org/user/{*ofname*}">OpenFlights</a>!',
-				 'template_body' => 'Including the flight from <b>{*src*}</b> to <b>{*dst*}</b>, the flights cover a distance of <b>{*distance*}</b> miles.');
+$full_story_template = array('template_title' => '{*actor*} added <b>{*count*}</b> new flights to <fb:pronoun uid="actor" useyou="false" possessive="true"/> <a href="http://openflights.org/user/{*ofname*}">OpenFlights</a>!',
+				 'template_body' => 'Including the flight from <b>{*src*}</b> to <b>{*dst*}</b>, the flights span a distance of <b>{*distance*}</b> miles.');
 
 $action_links = array();
 $action_links[] = array('text' => 'See OpenFlights map',
@@ -28,7 +28,7 @@ try{
 							       $short_story_templates,
 							       $full_story_template,
 							       $action_links);
-  echo "Bundle ID: " . $result;
+  echo "Success!  Bundle ID: " . $result . "\n";
 
 }catch(FacebookRestClientException $e){
   echo "Exception: " . $e;
