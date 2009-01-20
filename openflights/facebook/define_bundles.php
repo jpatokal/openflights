736e8c351f02e94251c05eb21f0662db1a1a80d8
jpatokal <jpatokal@a57d767e-1c7c-476a-a379-f7d483c1f09d>
2009-01-20 06:29:07 +0000

    Remove bolding from titles

--- openflights/facebook/define_bundles.php
+++ openflights/facebook/define_bundles.php
@@ -8,14 +8,14 @@ $facebook = new Facebook($appapikey, $appsecret);
 $facebook->api_client->session_key = $infinitesessionkey;
 
 $one_line_story_templates = array();
-$one_line_story_templates[] = '{*actor*} added <b>{*count*}</b> new flights, including a flight from <b>{*src*}</b> to <b>{*dst*}</b>, to <fb:pronoun uid="actor" useyou="false" possessive="true"/> <a href="http://openflights.org/user/{*ofname*}">OpenFlights</a>!';
-$one_line_story_templates[] = '{*actor*} added a flight from <b>{*src*}</b> to <b>{*dst*}</b> to <fb:pronoun uid="actor" useyou="false" possessive="true"/> <a href="http://openflights.org/user/{*ofname*}">OpenFlights</a>!';
+$one_line_story_templates[] = '{*actor*} added {*count*} new flights, including a flight from {*src*} to {*dst*}, to <fb:pronoun uid="actor" useyou="false" possessive="true"/> <a href="http://openflights.org/user/{*ofname*}">OpenFlights</a>!';
+$one_line_story_templates[] = '{*actor*} added new flights to <fb:pronoun uid="actor" useyou="false" possessive="true"/> <a href="http://openflights.org/user/{*ofname*}">OpenFlights</a>!';
 
 $short_story_templates = array();
-$short_story_templates[] = array('template_title' => '{*actor*} added <b>{*count*}</b> new flights to <fb:pronoun uid="actor" useyou="false" possessive="true"/> <a href="http://openflights.org/user/{*ofname*}">OpenFlights</a>!',
+$short_story_templates[] = array('template_title' => '{*actor*} added {*count*} new flights to <fb:pronoun uid="actor" useyou="false" possessive="true"/> <a href="http://openflights.org/user/{*ofname*}">OpenFlights</a>!',
 				 'template_body' => 'Including the flight from <b>{*src*}</b> to <b>{*dst*}</b>, the flights span a distance of <b>{*distance*}</b> miles.');
 
-$full_story_template = array('template_title' => '{*actor*} added <b>{*count*}</b> new flights to <fb:pronoun uid="actor" useyou="false" possessive="true"/> <a href="http://openflights.org/user/{*ofname*}">OpenFlights</a>!',
+$full_story_template = array('template_title' => '{*actor*} added {*count*} new flights to <fb:pronoun uid="actor" useyou="false" possessive="true"/> <a href="http://openflights.org/user/{*ofname*}">OpenFlights</a>!',
 				 'template_body' => 'Including the flight from <b>{*src*}</b> to <b>{*dst*}</b>, the flights span a distance of <b>{*distance*}</b> miles.');
 
 $action_links = array();
