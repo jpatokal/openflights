23381dfdbe9b6326c4c0e7ffacee10960a75b286
jpatokal <jpatokal@a57d767e-1c7c-476a-a379-f7d483c1f09d>
2009-01-20 05:16:26 +0000

    Use bundled feed system

--- openflights/facebook/updater.php
+++ openflights/facebook/updater.php
@@ -4,7 +4,10 @@ require_once 'php/facebook.php';
 require_once 'keys.php';
 require_once 'profile.php';
 
+// Defined in keys.php, see http://www.facebook.com/topic.php?uid=2205007948&topic=5350 to generate your own
 $facebook = new Facebook($appapikey, $appsecret);
+$facebook->api_client->session_key = $infinitesessionkey;
+
 $db = mysql_connect("localhost", "openflights");
 mysql_select_db("flightdb",$db);
 $fbupdates = 0;
@@ -15,32 +18,32 @@ $sql = "SELECT fb.uid,fbuid,u.name,COUNT(*) AS count,SUM(distance) AS distance,f
 $result = mysql_query($sql, $db);
 while($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
   $count = $row["count"];
+
+  // This guy has
   if($count > 0) {
     $updated = $row["updated"];
     $uid = $row["uid"];
     $fbuid = $row["fbuid"];
     $ofname = $row["name"];
+
+    // Get details of last flight entered
     $sql = "SELECT s.city AS src, d.city AS dst FROM flights AS f,airports AS s,airports AS d WHERE f.uid=$uid AND f.src_apid=s.apid AND f.dst_apid=d.apid AND f.upd_time > '$updated' ORDER BY f.upd_time LIMIT 1";
     $detailresult = mysql_query($sql, $db);
     if($detail = mysql_fetch_array($detailresult, MYSQL_ASSOC)) {
-      $feed_title = '<fb:name uid=\"$fbuid\" useyou=\"false\" /> has added a flight from <b>' . $detail["src"] . '</b> to <b>' . $detail["dst"] . '</b>';
-      if($count > 1) {
-	$feed_title .= " and <b>$count</b> more";
-      }
-      $feed_title .= ', covering ' . $row["distance"] . ' miles, to <fb:pronoun uid=\"$fbuid\" useyou=\"false\" possessive=\"true\"> <a href="http://openflights.org/user/' . $ofname . '">OpenFlights!</a>';
+      $tokens = array( 'src' => $detail["src"],
+		       'dst' => $detail["dst"],
+		       'count' => $detail["count"] - 1,
+		       'ofname' => $ofname );
+      $target_ids = array();
+      $body_general = '';
+
       try{
 	// Publish feed story
-	$feed_body = 'Take a look at <a href="http://openflights.org/user/' . $ofname . '">the updated map</a>.';
-	$facebook->api_client->feed_publishActionOfUser($feed_title, $feed_body);
-	
-	echo $feed_title . "\n";
-	echo $feed_body . "\n";
+	$facebook->api_client->feed_publishUserAction( $template_bundle_id, json_encode($tokens) , implode(',', $target_ids), $body_general);
 
 	// Update the user's profile box
 	$profile_box = get_profile($db, $uid, $fbuid, $ofname);
 	$facebook->api_client->profile_setFBML(null, $fbuid, null, null, null, $profile_box);
-
-	echo $profile_box;
 
 	// Mark user as updated
 	$sql = "UPDATE facebook SET updated=NOW() WHERE uid=$uid";
