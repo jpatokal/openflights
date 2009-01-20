dc610dbcd6e70282f15cf8fba40c43488324e436
jpatokal <jpatokal@a57d767e-1c7c-476a-a379-f7d483c1f09d>
2009-01-20 14:46:29 +0000

    Infinite sessions for each user separately

--- openflights/facebook/updater.php
+++ openflights/facebook/updater.php
@@ -4,17 +4,14 @@ require_once 'php/facebook.php';
 require_once 'keys.php';
 require_once 'profile.php';
 
-// Defined in keys.php, see http://www.facebook.com/topic.php?uid=2205007948&topic=5350 to generate your own
 $facebook = new Facebook($appapikey, $appsecret);
-$facebook->api_client->session_key = $infinitesessionkey;
-
 $db = mysql_connect("localhost", "openflights");
 mysql_select_db("flightdb",$db);
 $fbupdates = 0;
 $fbfail = 0;
 
-// Check which FB users have added flights
-$sql = "SELECT fb.uid,fbuid,u.name,COUNT(*) AS count,SUM(distance) AS distance,fb.updated FROM flights AS f,facebook AS fb, users AS u WHERE f.uid=fb.uid AND u.uid=fb.uid AND f.upd_time > fb.updated GROUP BY f.uid";
+// Check which FB users have valid infinite session keys and new flights since last update
+$sql = "SELECT fb.uid,fb.session,fbuid,u.name,COUNT(*) AS count,SUM(distance) AS distance,fb.updated FROM flights AS f,facebook AS fb, users AS u WHERE fb.session IS NOT NULL AND f.uid=fb.uid AND u.uid=fb.uid AND f.upd_time > fb.updated GROUP BY f.uid";
 $result = mysql_query($sql, $db);
 while($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
   $count = $row["count"];
@@ -25,6 +22,7 @@ while($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
     $uid = $row["uid"];
     $fbuid = $row["fbuid"];
     $ofname = $row["name"];
+    $infinitesessionkey = $row["session"];
 
     // Get details of last flight entered
     $sql = "SELECT s.city AS src, d.city AS dst FROM flights AS f,airports AS s,airports AS d WHERE f.uid=$uid AND f.src_apid=s.apid AND f.dst_apid=d.apid AND f.upd_time > '$updated' ORDER BY f.upd_time LIMIT 1";
@@ -39,7 +37,11 @@ while($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
       $body_general = '';
 
       try{
+	// Use this user's session key
+	$facebook->api_client->session_key = $infinitesessionkey;
+
 	// Publish feed story
+	// NOTE: template_bundle_id must already be created by define_bundles.php and stored in keys.php
 	$facebook->api_client->feed_publishUserAction( $template_bundle_id, json_encode($tokens) , implode(',', $target_ids), $body_general);
 
 	// Update the user's profile box
