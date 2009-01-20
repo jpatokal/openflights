87a6102c4ab7c3ab3771bc225a2a14382080b006
jpatokal <jpatokal@a57d767e-1c7c-476a-a379-f7d483c1f09d>
2009-01-20 07:49:16 +0000

    Add tabs Store OFname in Facebook storage

--- openflights/facebook/index.php
+++ openflights/facebook/index.php
@@ -9,49 +9,63 @@ require_once 'profile.php';
 $facebook = new Facebook($appapikey, $appsecret);
 $fbuid = $facebook->require_login();
 
-// You need to set info or profile box in order for the button's below to show up.
-// Don't set them every time.
-$is_set = $facebook->api_client->data_getUserPreference(1);
-
-if($_REQUEST["ofname"]){
+// Has the user configured their OpenFlights name?
+$ofname = $facebook->api_client->data_getUserPreference(1);
+if(! $ofname || $ofname == "") {
+  // Nope, did they just submit it?
   $ofname = $_REQUEST["ofname"];
-} else {
-  echo "<form requirelogin=\"1\">";
-  echo "<h2>Configuration</h2>";
-  echo "<p>Please enter your username on OpenFlights: <input type='text' name='ofname' value='$ofname' /></p>";
-  echo "<input type='submit' value='Submit' />";
+  if($ofname) {
+    // Yes, check it
+    $db = mysql_connect("localhost", "openflights");
+    mysql_select_db("flightdb",$db);
+    $sql = "SELECT public, uid FROM users WHERE name='" . $ofname . "'";
+    $result = mysql_query($sql, $db);
+    if($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+      if($row["public"] == "N") {
+	die("Sorry, $ofname's profile is set to 'Private'.  Please go to 'Settings', change it to 'Public' or 'Open', and try again.");
+      }
+    } else {
+      die("Sorry, couldn't find <b>$ofname</b> at OpenFlights.  Please check the spelling, hit 'Back' and try again.");
+    }
 
-  echo "<p>If you don't have an account already, you can <a href='http://openflights.org/html/signup.html'>sign up</a> for one now.</p>";
+    echo("<p><b>Thank you!</b>  Setting up your profile...</p>");
 
-  echo "</form>";
-  return;
+    // Looking good, save to Facebook and our internal table
+    $facebook->api_client->data_setUserPreference(1, $ofname);
+
+    $uid = $row["uid"];
+    $sql = sprintf("INSERT INTO facebook(uid,fbuid,updated) VALUES(%s,%s,NOW())", $uid, $fbuid);
+    $result = mysql_query($sql, $db);
+
+    // Update the user's profile box
+    $profile_box = get_profile($db, $uid, $fbuid, $ofname);
+    echo $profile_box;
+    $facebook->api_client->profile_setFBML(null, $fbuid, null, null, null, $profile_box);
+
+    echo("<p>Done!</p>");
+
+  }
+ } else {
+    // No, ask for it
+    echo "<form requirelogin=\"1\">";
+    echo "<h2>Configuration</h2>";
+    echo "<p>Please enter your username on OpenFlights: <input type='text' name='ofname' value='$ofname' /></p>";
+    echo "<input type='submit' value='Submit' />";
+
+    echo "<p>If you don't have an account already, you can <a href='http://openflights.org/html/signup.html'>sign up</a> for one now.</p>";
+
+    echo "</form>";
+    return;
+  }
  }
 
-$db = mysql_connect("localhost", "openflights");
-mysql_select_db("flightdb",$db);
-$sql = "SELECT public, uid FROM users WHERE name='" . $ofname . "'";
-$result = mysql_query($sql, $db);
-if($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-  if($row["public"] == "N") {
-    die("Sorry, $ofname's profile is set to 'Private'.  Please go to 'Settings', change it to 'Public' or 'Open', and try again.");
-  }
-} else {
-  die("Sorry, couldn't find <b>$ofname</b> at OpenFlights.  Please hit 'Back' and try again.");
-}
-$uid = $row["uid"];
+?>
 
-// Add this config into our table of Facebook users
-$sql = sprintf("INSERT INTO facebook(uid,fbuid,updated) VALUES(%s,%s,NOW())", $uid, $fbuid);
-$result = mysql_query($sql, $db);
+<fb:tabs>
+	<fb:tab-item href="http://apps.facebook.com/openflights/index.php" title="Home" selected="true"/>;
+	<fb:tab-item href="http://apps.facebook.com/openflights/invite.php" title="Invite Friends"/>;
+</fb:tabs>
 
-echo 'Click below to add the OpenFlights box to your Facebook profile.';
-echo '<div class="section_button"><fb:add-section-button section="profile"/></div>';
-
-echo 'And click here if you want to post new flights to Facebook automatically.';
-echo '<fb:prompt-permission perms="offline_access">Grant permission for automatic updates</fb:prompt-permission>'
-
-// Update the user's profile box
-
-$profile_box = get_profile($db, $uid, $fbuid, $ofname);
-echo $profile_box;
-$facebook->api_client->profile_setFBML(null, $fbuid, null, null, null, $profile_box);
+<p>Click the button below to add the OpenFlights box to your Facebook profile.</p>
+<div class="section_button"><fb:add-section-button section="profile"/></div>
+<p>(If there is no button, it was added already.)</p>
