92e6b49f6a202561937a0f6eda86ba4b8d1de403
Jani Patokallio <jpatokal@users.noreply.github.com>
2009-01-23 10:17:49 +0000

    Filter out "Unknown"/"Private" flights

--- sql/top10.sql
+++ sql/top10.sql
@@ -20,7 +20,7 @@ select lpad(count,8," ") as '</pre></td></tr><tr><td style="vertical-align: top;
 
 select distinct CONCAT(s.iata,' &harr;') as '</pre></td></tr><tr><td style="vertical-align: top;"><pre><b>Top 10 routes</b>',d.iata as '',lpad(count(fid),6," ") as '' from airports as s,airports as d,flights as f where f.uid != 1 and s.apid=f.src_apid and d.apid=f.dst_apid group by s.apid,d.apid order by count(fid) desc limit 10;
 
-select rpad(a.name,20,' ') as '</pre></td><td style="vertical-align: top;"><pre><b>Top 10 airlines</b>', lpad(count(*),6," ") as '' from airlines as a,flights as f where f.uid != 1 and a.alid=f.alid group by f.alid order by count(*) desc limit 10;
+select rpad(a.name,20,' ') as '</pre></td><td style="vertical-align: top;"><pre><b>Top 10 airlines</b>', lpad(count(*),6," ") as '' from airlines as a,flights as f where f.uid != 1 and a.alid > 1 and a.alid=f.alid group by f.alid order by count(*) desc limit 10;
 
 select rpad(a.name,20,' ') as '</pre></td><td style="vertical-align: top;"><pre><b>Top 10 airports</b>',a.iata as '', lpad(count(*),6," ") as '' from airports as a,flights as f where f.uid != 1 and a.apid=f.src_apid or a.apid=f.dst_apid group by a.apid order by count(*) desc limit 10;
 
