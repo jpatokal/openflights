9ceca0961a219c9989bbf0a348fc94ee6c69a6f3
Jani Patokallio <jpatokal@users.noreply.github.com>
2009-02-22 11:02:47 +0000

    * "Analyze" now includes count of countries, avg length, duration * php/stats.php partly JSON-ified * "Trip" filter prominence reduced * Clearer "Help" icons * "X" icon now OpenFlights Purple(tm)

--- php/stats.php
+++ php/stats.php
@@ -56,21 +56,22 @@ if($user && $user != "0") {
   }
 }
 $filter = "f.uid=" . $uid . getFilterString($HTTP_POST_VARS);
+$array = array();
 
-// unique airports
-$sql = "SELECT COUNT(*) AS num_airports FROM (SELECT src_apid FROM flights AS f WHERE " . $filter . " UNION SELECT dst_apid FROM flights AS f WHERE " . $filter . ") AS FOO";
+// unique airports, and countries
+$sql = "SELECT COUNT(DISTINCT a.apid) AS num_airports, COUNT(DISTINCT a.country) AS num_countries FROM flights AS f,airports AS a WHERE (f.src_apid=a.apid OR f.dst_apid=a.apid) AND " . $filter;
 $result = mysql_query($sql, $db);
 if($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-  printf ("%s,", $row["num_airports"]);
+  $array += $row;
 }
 
-// unique airlines, total distance
-$sql = "SELECT COUNT(DISTINCT alid) AS num_airlines, COUNT(DISTINCT plid) AS num_planes, SUM(distance) AS distance FROM flights AS f WHERE " . $filter;
+// unique airlines, unique planes, total distance, average distance, average duration
+$sql = "SELECT COUNT(DISTINCT alid) AS num_airlines, COUNT(DISTINCT plid) AS num_planes, SUM(distance) AS distance, ROUND(AVG(distance)) AS avg_distance, TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC(duration))/COUNT(duration)), '%H:%i') AS avg_duration FROM flights AS f WHERE " . $filter;
 $result = mysql_query($sql, $db);
 if($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-  printf ("%s,%s,%s", $row["num_airlines"], $row["num_planes"], $row["distance"]);
+  $array += $row;
 }
-printf ("\n");
+print json_encode($array) . "\n";
 
 // longest and shortest
 // 0 desc, 1 distance, 2 duration, 3 src_iata, 4 src_icao, 5 src_apid, 6 dst_iata, 7 dst_icao, 8 dst_apid
---
+++ test/stats.php
@@ -0,0 +1,34 @@
+<?php
+require_once(dirname(__FILE__) . '/simpletest/autorun.php');
+require_once(dirname(__FILE__) . '/simpletest/web_tester.php');
+include_once(dirname(__FILE__) . '/config.php');
+
+//
+// Test cases for php/stats.php
+// NB: Assumes the test user exists and flights.php has been run, so that $flight2[] is already in DB
+//
+
+$fid = null; // global for newly-added flight
+
+// Check Analyse stats
+class CheckAnalyseStats extends WebTestCase {
+  function test() {
+    global $webroot, $settings, $flight2, $fid;
+
+    login($this);
+    $this->assertText("1;");
+
+    $stats = $this->post($webroot . "php/stats.php");
+
+    // Uniques
+    $this->assertText('"num_airports":"2"');
+    $this->assertText('"num_countries":"1"');
+    $this->assertText('"num_airlines":"1"');
+    $this->assertText('"num_planes":"1"');
+    $this->assertText('"distance":"' . $flight2["distance"] . '"');
+    $this->assertText('"avg_distance":"' . $flight2["distance"] . '"');
+    $this->assertText('"avg_duration":"' . $flight2["duration"] . '"');
+  }
+}
+
+?>
