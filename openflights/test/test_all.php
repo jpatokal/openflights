5c2ea9611db86b636493f0e32ff55af43953bae4
jpatokal <jpatokal@a57d767e-1c7c-476a-a379-f7d483c1f09d>
2009-03-04 09:39:26 +0000

    * Top 10 allows sorting by flight count or distance (2168185) * Top 10 can also show top 20, 50 flights (2495106) * Top 10 CSS rewritten (use padding instead of border-spacing), works better on IE * Top 10 test cases added

--- openflights/test/test_all.php
+++ openflights/test/test_all.php
@@ -24,15 +24,22 @@ $test = &new TestSuite('Analyse (Stats)');
 $test->addTestFile('stats.php');
 $test->run(new HtmlReporter());
 
+// Top 10 stats
+$test = &new TestSuite('Top 10');
+$test->addTestFile('top10.php');
+$test->run(new HtmlReporter());
+
 // Add airport
 $test = &new TestSuite('Airport Search');
 $test->addTestFile('apsearch.php');
 $test->run(new HtmlReporter());
 
+// Autocompletion results
 $test = &new TestSuite('Autocomplete');
 $test->addTestFile('autocomplete.php');
 $test->run(new HtmlReporter());
 
+// ...and cleanup after all that
 $test = &new TestSuite('Cleanup');
 $test->addTestFile('cleanup.php');
 $test->run(new HtmlReporter());
