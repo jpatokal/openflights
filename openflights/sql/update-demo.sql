abe85500affcb948a34e4c6ba535c6ff7e1da1b9
jpatokal <jpatokal@a57d767e-1c7c-476a-a379-f7d483c1f09d>
2009-03-26 15:22:38 +0000

    Demo now copies flight date, mode

--- openflights/sql/update-demo.sql
+++ openflights/sql/update-demo.sql
@@ -4,5 +4,5 @@
 delete from flights where uid=1;
 -- Select every other flight
 -- Note: uid forced to 0 (demo), trid forced to NULL (not in trip)
-insert into flights(uid,src_apid,src_time,dst_apid,distance,code,seat,seat_type,class,reason,plid,alid,trid,duration,registration,note,upd_time,opp) select 1,src_apid,src_time,dst_apid,distance,code,seat,seat_type,class,reason,plid,alid,null,duration,registration,note,upd_time,opp from flights where fid % 2 = 0 and date_sub(now(), interval 3 day) < upd_time order by upd_time desc limit 200;
+insert into flights(uid,src_apid,src_date,src_time,dst_apid,distance,code,seat,seat_type,class,reason,plid,alid,trid,duration,registration,note,upd_time,opp,mode) select 1,src_apid,src_date,src_time,dst_apid,distance,code,seat,seat_type,class,reason,plid,alid,null,duration,registration,note,upd_time,opp,mode from flights where fid % 2 = 0 and date_sub(now(), interval 3 day) < upd_time order by upd_time desc limit 200;
 select now(), count(*) from flights where uid=1;
