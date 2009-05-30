e276182a7c703d926a337b60b09a55345cc47b59
jpatokal <jpatokal@a57d767e-1c7c-476a-a379-f7d483c1f09d>
2009-05-30 16:46:33 +0000

    Strip out location rows

--- openflights/locale/gettext.sh
+++ openflights/locale/gettext.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-# Collect gettext strings from all PHP files and update existing .po files
+# Collect gettext strings from all PHP/JavaScript files and update existing .po files
 # GNU xgettext does not recognize JavaScript, but oddly enough C seems to parse it (almost) fine...
 # 
-xgettext --omit-header -j -o locale/fi_FI.utf8/LC_MESSAGES/messages.po php/*php html/*php
-xgettext --omit-header -L C -j -o locale/fi_FI.utf8/LC_MESSAGES/messages.po js/apsearch.js
+xgettext --omit-header --no-location -j -o locale/fi_FI.utf8/LC_MESSAGES/messages.po php/*php html/*php
+xgettext --omit-header --no-location -L C -j -o locale/fi_FI.utf8/LC_MESSAGES/messages.po js/apsearch.js
 
-xgettext --omit-header -j -o locale/ja_JP.utf8/LC_MESSAGES/messages.po php/*php html/*php
-xgettext --omit-header -L C -j -o locale/fi_FI.utf8/LC_MESSAGES/messages.po js/apsearch.js
+xgettext --omit-header --no-location -j -o locale/ja_JP.utf8/LC_MESSAGES/messages.po php/*php html/*php
+xgettext --omit-header --no-location -L C -j -o locale/fi_FI.utf8/LC_MESSAGES/messages.po js/apsearch.js
