5cb632d095b7e8383d925d5d7452e8bf2fd777fc
Jani Patokallio <jpatokal@users.noreply.github.com>
2009-06-01 09:54:01 +0000

    Locale pulldown generator into function

--- php/locale.php
+++ php/locale.php
@@ -14,4 +14,22 @@ $locale .= ".utf8";
 setlocale(LC_ALL, $locale);
 bindtextdomain("messages", "../locale");
 textdomain("messages");
+
+//
+// Generate select box (pulldown) with all known locales
+// Box ID is "locale" and it triggers JS changeLocale() when selection is changed
+// $db -- OpenFlights DB
+// $locale -- currently selected locale
+//
+function locale_pulldown($db, $locale) {
+  echo "<select id='locale' onChange='JavaScript:changeLocale()'";
+  $sql = "SELECT * FROM locales ORDER BY name ASC";
+  $result = mysql_query($sql, $db);
+  while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    $selected = ($row["locale"] . ".utf8" == $locale ? "SELECTED" : "");
+    printf("<option value='%s' %s>%s (%s)</option>\n", $row["locale"], $selected, $row["name"], $row["locale"]);
+  }
+  echo "</select>";
+}
+
 ?>
