2cd2e4fed3545997c55e9cacea1267bd771d6416
Jani Patokallio <jpatokal@users.noreply.github.com>
2009-06-15 16:23:34 +0000

    Optimize slow query

--- sql/top10.sql
+++ sql/top10.sql
@@ -22,6 +22,11 @@ select distinct CONCAT(s.iata,' &harr;') as '</pre></td></tr><tr><td style="vert
 
 select rpad(a.name,20,' ') as '</pre></td><td style="vertical-align: top;"><pre><b>Top 10 airlines</b>', lpad(count(*),6," ") as '' from airlines as a,flights as f where f.uid != 1 and a.alid > 1 and a.alid=f.alid group by f.alid order by count(*) desc limit 10;
 
-select rpad(a.name,20,' ') as '</pre></td><td style="vertical-align: top;"><pre><b>Top 10 airports</b>',a.iata as '', lpad(count(*),6," ") as '' from airports as a,flights as f where f.uid != 1 and a.apid=f.src_apid or a.apid=f.dst_apid group by a.apid order by count(*) desc limit 10;
+select rpad(a.name,20,' ') as '</pre></td><td style="vertical-align: top;"><pre><b>Top 10 airports</b>',a.iata as '', lpad(count(*),6," ") as '' from airports as a,
+	(select src_apid as apid from flights where uid != 1
+	UNION ALL
+	select dst_apid as apid from flights where uid != 1) as f where f.apid=a.apid
+	group by a.apid
+	order by count(*) desc limit 10;
 
 select "</pre></td></tr></table>" AS "";
