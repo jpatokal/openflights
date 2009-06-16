61f6c61eb08a597485773747db3a467cd599b03e
jpatokal <jpatokal@a57d767e-1c7c-476a-a379-f7d483c1f09d>
2009-06-16 13:50:42 +0000

    Extra test case for filtered top 10 results

--- openflights/test/top10.php
+++ openflights/test/top10.php
@@ -42,6 +42,39 @@ class CheckTop10FlightCountStats extends WebTestCase {
   }
 }
 
+// Check flight count Top 10 stats with airline filtering
+class CheckTop10AirlineFilteredFlightCountStats extends WebTestCase {
+  function test() {
+    global $webroot, $settings, $flight2;
+
+    login($this);
+    $this->assertText("1;");
+
+    $filter = array("alid" => $flight2["alid"]);
+    $top10 = $this->post($webroot . "php/top10.php");
+    $rows = preg_split('/\n/', $top10);
+    $this->assertTrue(sizeof($rows) == 4);
+
+    // Top 10 routes
+    $routes = $rows[0];
+    $this->assertPattern("/," . $flight2["src_apid"] . ",/", $routes);
+    $this->assertPattern("/," . $flight2["dst_apid"] . ",1/", $routes);
+
+    // Top 10 airports
+    $airports = $rows[1];
+    $this->assertPattern("/,1," . $flight2["src_apid"] . "/", $airports);
+    $this->assertPattern("/,1," . $flight2["dst_apid"] . "/", $airports);
+
+    // Top 10 airlines
+    $airlines = $rows[2];
+    $this->assertPattern("/,1," . $flight2["alid"] . "/", $airlines);
+
+    // Top 10 planes 
+    $planes = $rows[3];
+    $this->assertPattern("/" . $flight2["plane"] . ",1/", $planes);
+  }
+}
+
 // Check by-distance Top 10 stats
 class CheckTop10DistanceStats extends WebTestCase {
   function test() {
