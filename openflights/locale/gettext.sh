44a5905161cd9fc2258db5e381b85f0dd2222415
jpatokal <jpatokal@a57d767e-1c7c-476a-a379-f7d483c1f09d>
2009-07-02 02:24:02 +0000

    Final pre-launch tweaks

--- openflights/locale/gettext.sh
+++ openflights/locale/gettext.sh
@@ -6,7 +6,7 @@
 PHP="index.php php/*php html/*php"
 JS="openflights.js js/alsearch.js js/apsearch.js js/settings.js js/trip.js"
 
-for lang in de_DE fi_FI fr_FR ja_JP lt_LT sv_SE ru_RU; do
+for lang in de_DE fi_FI fr_FR ja_JP lt_LT pl_PL sv_SE ru_RU; do
   echo $lang
   xgettext --omit-header --no-location --no-wrap -j -o locale/$lang.utf8/LC_MESSAGES/messages.po $PHP
   xgettext --omit-header --no-location  --no-wrap -L C -j -o locale/$lang.utf8/LC_MESSAGES/messages.po $JS
