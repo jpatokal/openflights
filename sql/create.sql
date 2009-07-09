6e77f1b9117aef1ca5142422520911e7b42f8354
Jani Patokallio <jpatokal@users.noreply.github.com>
2009-07-09 15:57:58 +0000

    Brought up to date

--- sql/create.sql
+++ sql/create.sql
@@ -1,93 +1,186 @@
-CREATE DATABASE flightdb;
+CREATE DATABASE flightdb2;
 
 CREATE USER openflights;
-GRANT ALL PRIVILEGES ON flightdb.* TO openflights;
+GRANT ALL PRIVILEGES ON flightdb2.* TO openflights;
 
-CONNECT flightdb;
+CONNECT flightdb2;
 
-CREATE TABLE locales
-(
-locale VARCHAR(5) PRIMARY KEY,
-name TEXT
-);
+CREATE TABLE `locales` (
+  `locale` varchar(5) NOT NULL,
+  `name` mediumtext,
+  PRIMARY KEY  (`locale`)
+) DEFAULT CHARSET=utf8;
 
-CREATE TABLE users
-(
-name TEXT UNIQUE KEY,
-password TEXT,
-email TEXT,
-public TEXT,
-uid INT AUTO_INCREMENT PRIMARY KEY
-);
+CREATE TABLE `users` (
+  `name` text,
+  `password` text,
+  `uid` int(11) NOT NULL auto_increment,
+  `public` text,
+  `email` text,
+  `count` int(11) default '0',
+  `editor` varchar(1) default 'B',
+  `elite` varchar(1) default '',
+  `validity` date default NULL,
+  `guestpw` text,
+  `startpane` varchar(1) default 'H',
+  `locale` varchar(5) default 'en_US',
+  PRIMARY KEY  (`uid`)
+) DEFAULT CHARSET=utf8;
 
-CREATE TABLE airports_gad
-(
-name TEXT NOT NULL,
-city TEXT,
-country TEXT,
-iata VARCHAR(3),
-icao VARCHAR(4),
-x DOUBLE NOT NULL,
-y DOUBLE NOT NULL,
-elevation INT,
-apid INT AUTO_INCREMENT PRIMARY KEY
-);
+CREATE TABLE `airports` (
+  `name` text NOT NULL,
+  `city` text,
+  `country` text,
+  `iata` varchar(3) default NULL,
+  `icao` varchar(4) default NULL,
+  `x` double NOT NULL,
+  `y` double NOT NULL,
+  `elevation` int(11) default NULL,
+  `apid` int(11) NOT NULL auto_increment,
+  `uid` int(11) default NULL,
+  `timezone` float default NULL,
+  `dst` varchar(1) default NULL,
+  PRIMARY KEY  (`apid`),
+  KEY `y` (`y`),
+  KEY `x` (`x`),
+  KEY `iata` (`iata`)
+) DEFAULT CHARSET=utf8;
 
-CREATE TABLE planes
-(
-name TEXT,
-abbr TEXT,
-speed DOUBLE,
-plid INT AUTO_INCREMENT PRIMARY KEY
-);
+CREATE TABLE `airports_dafif` (
+  `name` text NOT NULL,
+  `city` text,
+  `code` text,
+  `iata` varchar(3) default NULL,
+  `icao` varchar(4) NOT NULL,
+  `x` double NOT NULL,
+  `y` double NOT NULL,
+  `elevation` int(11) default NULL,
+  PRIMARY KEY  (`icao`)
+) DEFAULT CHARSET=utf8;
 
-CREATE TABLE airlines
-(
-name TEXT,
-iata VARCHAR(2),
-icao VARCHAR(3),
-callsign TEXT,
-country TEXT,
-alid INT AUTO_INCREMENT PRIMARY KEY
-);
+CREATE TABLE `airports_gad` (
+  `name` text NOT NULL,
+  `city` text,
+  `country` text,
+  `iata` varchar(3) default NULL,
+  `icao` varchar(4) NOT NULL,
+  `x` double NOT NULL,
+  `y` double NOT NULL,
+  `elevation` int(11) default NULL,
+  PRIMARY KEY  (`icao`)
+) DEFAULT CHARSET=utf8;
 
-CREATE TABLE trips
-(
-name TEXT,
-url TEXT,
-uid INT,
-public TEXT,
-trid INT AUTO_INCREMENT PRIMARY KEY,
-FOREIGN KEY (uid) REFERENCES users (uid)
-);
+CREATE TABLE `airports_oa` (
+  `name` text NOT NULL,
+  `city` text,
+  `country` text,
+  `continent` text,
+  `iata` varchar(3) default NULL,
+  `icao` varchar(4) default NULL,
+  `x` double NOT NULL,
+  `y` double NOT NULL,
+  `elevation` int(11) default NULL,
+  `ident` text,
+  `type` text,
+  `region` text,
+  `service` text,
+  `gps` text,
+  `keywords` text,
+  `code` varchar(2) default NULL,
+  KEY `iata` (`iata`)
+) DEFAULT CHARSET=utf8;
 
-CREATE TABLE flights
-(
-uid INT,
-src_apid INT,
-dst_apid INT,
-src_time DATETIME,
-duration TIME,
-distance INT,
-code TEXT,
-seat TEXT,
-seat_type TEXT,
-class TEXT,
-reason TEXT,
-registration TEXT,
-note TEXT,
-upd_time DATETIME,
-plid INT,
-alid INT,
-trid INT,
-fid INT AUTO_INCREMENT PRIMARY KEY,
-FOREIGN KEY (uid) REFERENCES users (uid),
-FOREIGN KEY (src_apid) REFERENCES airports (apid),
-FOREIGN KEY (dst_apid) REFERENCES airports (apid),
-FOREIGN KEY (plid) REFERENCES planes (plid),
-FOREIGN KEY (alid) REFERENCES airlines (alid),
-FOREIGN KEY (trid) REFERENCES trips (trid)
-);
+CREATE TABLE `planes` (
+  `name` text,
+  `abbr` text,
+  `speed` double default NULL,
+  `plid` int(11) NOT NULL auto_increment,
+  `public` char(1) default 'N',
+  PRIMARY KEY  (`plid`)
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE `airlines` (
+  `name` text,
+  `iata` varchar(2) default NULL,
+  `icao` varchar(3) default NULL,
+  `callsign` text,
+  `country` text,
+  `alid` int(11) NOT NULL auto_increment,
+  `uid` int(11) default NULL,
+  `alias` text,
+  `mode` char(1) default 'F',
+  PRIMARY KEY  (`alid`),
+  KEY `iata` (`iata`)
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE `routes` (
+  `airline` varchar(2) default NULL,
+  `alid` int(11) default NULL,
+  `src_ap` varchar(3) default NULL,
+  `src_apid` int(11) default NULL,
+  `dst_ap` varchar(3) default NULL,
+  `dst_apid` int(11) default NULL,
+  `codeshare` text,
+  `stops` text,
+  `equipment` text,
+  `rid` int(11) NOT NULL auto_increment,
+  PRIMARY KEY  (`rid`),
+  KEY `src_apid` (`src_apid`),
+  KEY `dst_apid` (`dst_apid`),
+  KEY `alid` (`alid`)
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE `trips` (
+  `name` text,
+  `url` text,
+  `uid` int(11) default NULL,
+  `trid` int(11) NOT NULL auto_increment,
+  `public` text,
+  PRIMARY KEY  (`trid`),
+  KEY `uid` (`uid`)
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE `flights` (
+  `uid` int(11) default NULL,
+  `src_apid` int(11) default NULL,
+  `src_time` time default NULL,
+  `dst_apid` int(11) default NULL,
+  `distance` int(11) default NULL,
+  `code` text,
+  `seat` text,
+  `seat_type` text,
+  `class` text,
+  `reason` text,
+  `plid` int(11) default NULL,
+  `alid` int(11) default NULL,
+  `trid` int(11) default NULL,
+  `fid` int(11) NOT NULL auto_increment,
+  `duration` time default NULL,
+  `registration` text,
+  `note` text,
+  `upd_time` datetime default NULL,
+  `opp` char(1) default 'N',
+  `src_date` date default NULL,
+  `mode` char(1) default 'F',
+  PRIMARY KEY  (`fid`),
+  KEY `src_apid` (`src_apid`),
+  KEY `dst_apid` (`dst_apid`),
+  KEY `plid` (`plid`),
+  KEY `alid` (`alid`),
+  KEY `trid` (`trid`),
+  KEY `uid` (`uid`)
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE `facebook` (
+  `uid` int(11) default NULL,
+  `fbuid` int(11) NOT NULL,
+  `updated` datetime default NULL,
+  `sessionkey` text,
+  `pref_onfly` varchar(1) default 'Y',
+  `pref_onnew` varchar(1) default 'Y',
+  PRIMARY KEY  (`fbuid`),
+  KEY `uid` (`uid`)
+) DEFAULT CHARSET=utf8;
 
 LOAD DATA LOCAL INFILE 'airlines.dat'
 INTO TABLE airlines
