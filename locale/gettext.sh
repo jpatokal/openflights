688d6d069f4c4978b58431d6647910b4430afaf2
Jani Patokallio <jpatokal@users.noreply.github.com>
2009-07-14 09:51:04 +0000

    New route map strings Implemented msgmerge to remove obsolete lines Finnish updated

--- locale/gettext.sh
+++ locale/gettext.sh
@@ -5,12 +5,21 @@
 # 
 PHP="index.php php/*php html/*php"
 JS="openflights.js js/alsearch.js js/apsearch.js js/settings.js js/trip.js"
-
+OPTS="--omit-header --no-location --no-wrap -j"
 for lang in de_DE fi_FI fr_FR ja_JP lt_LT pl_PL sv_SE ru_RU; do
+  POPATH=locale/$lang.utf8/LC_MESSAGES
   echo $lang
-  xgettext --omit-header --no-location --no-wrap -j -o locale/$lang.utf8/LC_MESSAGES/messages.po $PHP
-  xgettext --omit-header --no-location  --no-wrap -L C -j -o locale/$lang.utf8/LC_MESSAGES/messages.po $JS
+  touch $POPATH/new.po
+  xgettext $OPTS -o $POPATH/new.po $PHP
+  xgettext $OPTS -L C -o $POPATH/new.po $JS
+  msgmerge -N $POPATH/messages.po $POPATH/new.po >$POPATH/newest.po
+  mv $POPATH/newest.po $POPATH/messages.po
+  rm $POPATH/new.po
 done
 
-xgettext --omit-header --no-location --no-wrap -j -o locale/template.po $PHP
-xgettext --omit-header --no-location --no-wrap -L C -j -o locale/template.po $JS
+touch locale/new.po
+xgettext $OPTS -o locale/template.po $PHP
+xgettext $OPTS -L C -o locale/template.po $JS
+msgmerge -N locale/template.po locale/new.po >locale/newest.po
+mv locale/newest.po locale/template.po
+rm locale/new.po
