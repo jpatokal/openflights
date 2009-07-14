1dd67516e7eb05f6600b789fb62980e6bd1a1a6c
Jani Patokallio <jpatokal@users.noreply.github.com>
2009-07-14 16:19:47 +0000

    Bug fixes before airline route map launch

--- php/login.php
+++ php/login.php
@@ -26,7 +26,7 @@ if($name) {
     $_SESSION['editor'] = $myrow["editor"];
     $_SESSION['elite'] = $myrow["elite"];
     $_SESSION['units'] = $myrow["units"];
-    if($_SESSION['locale'] && $_SESSION['locale'] != $myrow["locale"]) {
+    if($myrow["locale"] != "en_US" && $_SESSION['locale'] != $myrow["locale"]) {
       $rc = 2; // force reload, so UI is changed into user's language
     } else {
       $rc = 1;
--- php/routes.php
+++ php/routes.php
@@ -37,13 +37,19 @@ if(substr($apid, 0, 1) == "L") {
   $type = "L";
   $apid = substr($apid, 1);
   $condition = "r.alid=$apid";
+  $codeshare = "codeshare";
 } else {
   $type = "A";
   $condition = "r.src_apid=$apid";
   if($alid) {
     $condition .= " AND r.alid=$alid";
+    $codeshare = "codeshare";
+  } else {
+    $codeshare = "'N'"; // never show dotted lines for airport route maps
   }
 }
+
+$map = "";
 
 // Title for this airport route data plus count of routes
 // (count = 0 when airport exists but has no routes)
@@ -59,11 +65,11 @@ if($type == "A") {
   if($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
     printf ("%s;%s;%s (<b>%s</b>)<br><small>%s, %s<br>%s routes</small>\n", $apid, $row["count"], $row["name"], format_apcode($row), $row["city"], $row["country"], $row["count"]);
   } else {
-    die('Error;No airport with ID $apid found');
+    die('Error;No airport with ID ' . $apid . ' found');
   }
   if($row["count"] == 0) {
     // No routes, print this airport and abort
-    printf("\n%s;%s;%s;%s;0;%s;N\n", format_apdata($row), $row["name"], $row["city"], $row["country"], format_airport($row));
+    $map = sprintf("\n%s;%s;%s;%s;0;%s;N\n", format_apdata($row), $row["name"], $row["city"], $row["country"], format_airport($row));
     exit;
   }
 } else {
@@ -73,7 +79,7 @@ if($type == "A") {
   if($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
     printf ("%s;%s;%s (<b>%s</b>)<br><small>%s</small><br>%s routes\n", "L" . $apid, $row["count"], $row["name"], $row["iata"], $row["country"], $row["count"]);
   } else {
-    die('Error;No airline with ID $apid found');
+    die('Error;No airline with ID ' . $apid . ' found');
   }
   if($row["count"] == 0) {
     // No routes, abort
@@ -84,8 +90,8 @@ if($type == "A") {
 }
 
 // List of all flights FROM this airport
-$sql = "SELECT DISTINCT s.apid,s.x,s.y,d.apid,d.x,d.y,count(rid),0,'N' AS future,'F' AS mode FROM routes AS r, airports AS s, airports AS d WHERE $condition AND r.src_apid=s.apid AND r.dst_apid=d.apid GROUP BY s.apid,d.apid";
-$result = mysql_query($sql, $db);
+$sql = "SELECT DISTINCT s.apid,s.x,s.y,d.apid,d.x,d.y,count(rid),0,$codeshare AS future,'F' AS mode FROM routes AS r, airports AS s, airports AS d WHERE $condition AND r.src_apid=s.apid AND r.dst_apid=d.apid GROUP BY s.apid,d.apid";
+$result = mysql_query($sql, $db) or die ('Error;Database error ' . $sql . ', ' . mysql_error());
 $first = true;
 while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
   $row[7] = gcPointDistance(array("x" => $row[1], "y" => $row[2]),
@@ -93,46 +99,54 @@ while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
   if($first) {
     $first = false;
   } else {
-    printf("\t");
+    $map .= "\t";
   }  
-  printf ("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s", $row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7], $row[8], $row[9]);
+  $map .= sprintf ("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s", $row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7], $row[8], $row[9]);
 }
-printf ("\n");
+$map .= "\n";
 
 // List of all airports with flights FROM this airport
-$sql = "SELECT DISTINCT a.apid,x,y,name,iata,icao,city,country,timezone,dst,count(name) AS visits,'N' AS future FROM routes AS r, airports AS a WHERE $condition AND (r.src_apid=a.apid OR r.dst_apid=a.apid) GROUP BY name ORDER BY visits ASC";
-$result = mysql_query($sql, $db);
+if($type == "A") {
+  $apcond = "(r.src_apid=a.apid OR r.dst_apid=a.apid)"; // include source airport
+} else {
+  $apcond = "r.src_apid=a.apid"; // prevent double-counting
+}
+
+$sql = "SELECT DISTINCT a.apid,x,y,name,iata,icao,city,country,timezone,dst,count(name) AS visits,$codeshare AS future FROM routes AS r, airports AS a WHERE $condition AND $apcond GROUP BY name ORDER BY visits ASC";
+$result = mysql_query($sql, $db) or die ('Error;Database error ' . $sql . ', ' . mysql_error());
 $first = true;
 while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
   if($first) {
     $first = false;
   } else {
-    printf("\t");
+    $map .= "\t";
   }
-  printf ("%s;%s;%s;%s;%s;%s;%s", format_apdata($row), $row["name"], $row["city"], $row["country"], $row["visits"], format_airport($row), $row["future"]);
+  $map .= sprintf ("%s;%s;%s;%s;%s;%s;%s", format_apdata($row), $row["name"], $row["city"], $row["country"], $row["visits"], format_airport($row), $row["future"]);
 }
 
 // Trips always null
-printf ("\n\n");
+$map .= "\n\n";
 
 // List of all airlines in this route map
 if($type == "L") {
   // Airline map obviously only has one airline...
-  printf("%s;%s", $apid, $alname);
+  $map .= sprintf("%s;%s", $apid, $alname);
  } else {
-  $sql = "SELECT DISTINCT a.alid, iata, icao, name FROM airlines as a, routes as r WHERE $condition AND a.alid=r.alid ORDER BY name;";
-  $result = mysql_query($sql, $db);
+  // Note: Existing airline filter is purposely ignored here
+  $sql = "SELECT DISTINCT a.alid, iata, icao, name FROM airlines as a, routes as r WHERE r.src_apid=$apid AND a.alid=r.alid ORDER BY name;";
+  $result = mysql_query($sql, $db) or die ('Error;Database error ' . $sql . ', ' . mysql_error());
   $first = true;
   while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
     if($first) {
       $first = false;
     } else {
-      printf("\t");
+      $map .= "\t";
     }  
-    printf ("%s;%s", $row["alid"], $row["name"]);
+    $map .= sprintf("%s;%s", $row["alid"], $row["name"]);
   }
 }
 
 // And years also null
-printf ("\n\n");
+print $map . "\n\n";
+
 ?>
