6cfbd0c69e46a09731c25363db87d1d0b14ec14c
Jani Patokallio <jpatokal@users.noreply.github.com>
2009-07-18 05:43:37 +0000

    Delete instead of just commenting out obsoleted strings

--- locale/gettext.sh
+++ locale/gettext.sh
@@ -12,14 +12,15 @@ for lang in de_DE es_ES fi_FI fr_FR ja_JP lt_LT pl_PL sv_SE ru_RU; do
   touch $POPATH/new.po
   xgettext $OPTS -o $POPATH/new.po $PHP
   xgettext $OPTS -L C -o $POPATH/new.po $JS
+  # Filter out obsoleted strings
   msgmerge -N $POPATH/messages.po $POPATH/new.po >$POPATH/newest.po
-  mv $POPATH/newest.po $POPATH/messages.po
-  rm $POPATH/new.po
+  grep -v "^#~" $POPATH/newest.po >$POPATH/messages.po
+  rm $POPATH/new.po $POPATH/newest.po
 done
 
 touch locale/new.po
 xgettext $OPTS -o locale/template.po $PHP
 xgettext $OPTS -L C -o locale/template.po $JS
 msgmerge -N locale/template.po locale/new.po >locale/newest.po
-mv locale/newest.po locale/template.po
-rm locale/new.po
+grep -v "^#~" locale/newest.po >locale/messages.po
+rm locale/new.po locale/newest.po
