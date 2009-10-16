bfa9a1003b695ce922afaaf2443f2ab0e519bcf3
jpatokal <jpatokal@a57d767e-1c7c-476a-a379-f7d483c1f09d>
2009-10-16 06:16:43 +0000

    Export only airlines

--- openflights/sql/export.sql
+++ openflights/sql/export.sql
@@ -3,25 +3,28 @@
 
 \! echo Airports...
 
-SELECT name,city,country,iata,icao,y,x,elevation,timezone,dst INTO OUTFILE '/tmp/airports.dat'
+SELECT apid,name,city,country,iata,icao,y,x,elevation,timezone,dst INTO OUTFILE '/tmp/airports.dat'
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 FROM airports;
 
 \! echo Airlines...
 
-SELECT name,alias,iata,icao,callsign,country INTO OUTFILE '/tmp/airlines.dat'
+SELECT alid,name,alias,iata,icao,callsign,country,active INTO OUTFILE '/tmp/airlines.dat'
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-FROM airlines;
+FROM airlines
+WHERE mode='F';
 
 \! echo Routes...
 
 SELECT airline,alid,src_ap,src_apid,dst_ap,dst_apid,codeshare,stops,equipment INTO OUTFILE '/tmp/routes.dat'
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
-FROM routes
+FROM routes;
 
+\! echo Waiting for dump to complete...
+\! sleep 5
 \! ls -l /tmp/*.dat
 \! cp /tmp/*.dat .
 
