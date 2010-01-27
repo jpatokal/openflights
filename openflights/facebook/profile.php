7766b313f0cabd7a2e7b1224f7f00cc76db35d2a
jpatokal <jpatokal@a57d767e-1c7c-476a-a379-f7d483c1f09d>
2010-01-27 14:08:08 +0000

    Final fix to km displaying code

--- openflights/facebook/profile.php
+++ openflights/facebook/profile.php
@@ -1,16 +1,16 @@
 <?php
-$KMPERMILE = "1.609344";
-
 // Generate content of Facebook profile box for given OF uid
 function get_profile($db, $uid, $fbuid, $ofname) {
+  $KMPERMILE = 1.609344;
   $sql = "SELECT COUNT(*) AS count, SUM(distance) AS distance, SUM(TIME_TO_SEC(duration))/60 AS duration, u.public, u.units FROM flights AS f, users AS u WHERE u.uid = f.uid AND f.uid=" . $uid . " GROUP BY f.uid";
   $result = mysql_query($sql, $db);
   if($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
     if($row["public"] == "N") {
       $content = sprintf("<b>Oops!</b>  Looks like <b><a href='http://openflights.org/user/%s'>%s</a></b>'s map has become private.", $ofname, $ofname);
     } else {
+      $distance = $row["distance"];
       if($row["units"] == "K") {
-	$distance = ($distance * $KMPERMILE) . " km";
+	$distance = round($distance * $KMPERMILE) . " km";
       } else {
 	$distance .= " miles";
       }
@@ -18,7 +18,7 @@ function get_profile($db, $uid, $fbuid, $ofname) {
 			  floor($row["duration"] / (60*24)),
 			  floor(($row["duration"] / 60 ) % 24),
 			  $row["duration"] % 60);
-      $content = sprintf("<b><a href='http://openflights.org/user/%s'>%s</a></b> (<fb:name uid=\"$fbuid\" useyou=\"false\" />) has flown <b>%s</b> times, for a total distance of <b>%s</b> and a total duration of <b>%s</b>!  <a href='http://openflights.org/user/%s'>Find out more.</a>", $ofname, $ofname, $row["count"], $row["distance"], $duration, $ofname);
+      $content = sprintf("<b><a href='http://openflights.org/user/%s'>%s</a></b> (<fb:name uid=\"$fbuid\" useyou=\"false\" />) has flown <b>%s</b> times, for a total distance of <b>%s</b> and a total duration of <b>%s</b>!  <a href='http://openflights.org/user/%s'>Find out more.</a>", $ofname, $ofname, $row["count"], $distance, $duration, $ofname);
     }
   } else {
     $content = sprintf("<b><a href='http://openflights.org/user/%s'>%s</a></b> (<fb:name uid=\"$fbuid\" useyou=\"false\" />) doesn't seem to have flown anywhere yet.   <a href='http://openflights.org/'>Add some flights?</a>", $ofname);
