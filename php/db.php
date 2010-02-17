ef2b7695f2da3e8c7ed73a9d5acd04c3f7fef152
Jani Patokallio <jpatokal@users.noreply.github.com>
2010-02-17 06:50:32 +0000

    Add sanity checks

--- php/db.php
+++ php/db.php
@@ -8,6 +8,11 @@ $password = "";
 $OF_ADMIN_UID = 3;
 
 $db = mysql_connect($host,$user,$password);
-mysql_select_db($dbname, $db);
+if (!$db) {
+  die('Error;Unable to connect to database: ' . mysql_error());
+}
+if(!mysql_select_db($dbname, $db)) {
+  die('Error;Unable to select database: ' . mysql_error());
+}
 mysql_query("SET NAMES 'utf8'");
 ?>
