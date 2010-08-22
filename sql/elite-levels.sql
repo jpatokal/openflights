7c4684e0192d64a5369b1c16fd07f77e649e9157
Jani Patokallio <jpatokal@users.noreply.github.com>
2010-08-22 08:06:27 +0000

    Optimize updating by using joins instead of subqueries

--- sql/elite-levels.sql
+++ sql/elite-levels.sql
@@ -2,12 +2,19 @@
 update users set elite="" where validity < now();
 
 -- Remove warning flag from users who have made their profiles public and have under 100 flights
-update users set elite="" where elite="X" and public != "N" and uid!= 1 and uid in
-	(select uid from flights group by uid having count(*) < 100);
+-- (LEFT JOIN ensures that users with 0 flights are included)
+update users LEFT JOIN (
+       select uid from flights group by uid having count(*) < 100
+) as nice_users
+on (users.uid = nice_users.uid)
+set elite="" where elite="X" and public != 'N' and users.uid!= 1;
 
 -- Set warning flag for non-elite users with >=100 flights
-update users set elite="X" where elite="" and uid!= 1 and uid in
-	(select uid from flights group by uid having count(*) >= 100);
+update users JOIN (
+       select uid from flights group by uid having count(*) >= 100
+) as naughty_users
+on (users.uid = naughty_users.uid)
+set elite="X" where elite="" and users.uid!= 1;
 
 -- Set warning flag for non-elite users with hidden profiles
 update users set elite="X" where elite="" and uid!= 1 and public="N";
