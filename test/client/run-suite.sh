2f760f7a82b4a81545bbbe8ac66d466508c71f90
Jani Patokallio <jpatokal@users.noreply.github.com>
2011-06-17 12:48:26 +0000

    Upgrade Selenium Server to 2.0 for Firefox 4 support

--- test/client/run-suite.sh
+++ test/client/run-suite.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
-echo Launching Selenium RC...
-java -jar ../selenium/selenium-server-1.0.3/selenium-server.jar >log/selenium.log &
+SELENIUM_SERVER=../selenium/selenium-server-standalone-2.0rc2.jar
+
+echo Launching Selenium Server...
+java -jar $SELENIUM_SERVER >log/selenium.log &
 
 echo Bootstrapping...
 php ../server/cleanup.php >>log/bootstrap.log
 php ../server/settings.php >log/bootstrap.log
 php ../server/submit.php >>log/bootstrap.log
 
-echo -n Waiting for Selenium to start
-for i in 1 2 3 4 5; do echo -n .; sleep 1; done
-echo
+echo -n Waiting for Selenium to start (this can take over a minute)
+while :; do
+  echo -n .
+  sleep 1
+  tail -1 log/selenium.log
+  if (tail -1 log/selenium.log | grep -q 'Started org.openqa.jetty.jetty.Server'); then
+    break
+  fi
+done
+
 phpunit --verbose .
 
 echo Cleaning up...
 php ../server/cleanup.php >>log/bootstrap.log
-kill `ps -ef | grep "selenium-server.jar$" | awk '{print $2}'`
+kill `ps -ef | grep "selenium-server.*$" | awk '{print $2}'`
 echo Done.
-
