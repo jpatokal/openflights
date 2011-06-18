4b578ca9e6a7a17861b512b1856cc16694a62446
jpatokal <jpatokal@a57d767e-1c7c-476a-a379-f7d483c1f09d>
2011-06-18 08:22:20 +0000

    Keep Selenium running, start only if necessary

--- openflights/test/client/run-suite.sh
+++ openflights/test/client/run-suite.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 SELENIUM_SERVER=../selenium/selenium-server-standalone-2.0rc2.jar
 
-echo Launching Selenium Server...
-java -jar $SELENIUM_SERVER >log/selenium.log &
-
 echo Bootstrapping...
 php ../server/cleanup.php >>log/bootstrap.log
 php ../server/settings.php >log/bootstrap.log
 php ../server/submit.php >>log/bootstrap.log
 
-echo -n Waiting for Selenium to start (this can take over a minute)
-while :; do
-  echo -n .
-  sleep 1
-  tail -1 log/selenium.log
-  if (tail -1 log/selenium.log | grep -q 'Started org.openqa.jetty.jetty.Server'); then
-    break
-  fi
-done
+if (ps -ef | grep -q "selenium-server.*jar$"); then
+  echo Selenium Server already running:
+  ps -ef | grep "selenium-server.*jar$"
+else
+  echo -n "Launching Selenium Server (this can take over a minute)"
+  java -jar $SELENIUM_SERVER >log/selenium.log &
+  while :; do
+    echo -n .
+    sleep 1
+    tail -1 log/selenium.log
+    if (tail -1 log/selenium.log | grep -q 'Started org.openqa.jetty.jetty.Server'); then
+      break
+    fi
+  done
+fi
 
 phpunit --verbose .
 
 echo Cleaning up...
 php ../server/cleanup.php >>log/bootstrap.log
-kill `ps -ef | grep "selenium-server.*$" | awk '{print $2}'`
 echo Done.
