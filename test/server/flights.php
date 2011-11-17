9c046d57fa037b65fa8edced6b2c68c9b58fcbd7
Jani Patokallio <jpatokal@users.noreply.github.com>
2011-11-17 10:38:58 +0000

    Bugfix: CSV exports of airlines, airports not possible

--- test/server/flights.php
+++ test/server/flights.php
@@ -17,4 +17,31 @@ class BlockAnonExportCase extends WebTestCase {
   }
 }
 
+class ExportAirlineToCSVCase extends WebTestCase {
+  function test() {
+    global $webroot, $route;
+
+    // First figure out the correct results
+    $db = db_connect();
+    $sql = "SELECT alid FROM airlines WHERE iata='" . $route["core_al_iata"] . "'";
+    $result = mysql_query($sql, $db) or die($sql . ":" . mysql_error());
+    if($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+      $alid = $row["alid"];
+    }
+
+    $sql = "SELECT COUNT(*) AS count FROM routes WHERE alid=" . $alid;
+    $result = mysql_query($sql, $db) or die($sql . ":" . mysql_error());
+    if($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+      $route_count = intval($row["count"]);
+    }
+
+    // Then test
+    assert_login($this);
+    $params = array("export" => "export", "id" => "L" . $alid);
+    $csv = $this->get($webroot . "php/flights.php", $params);
+    $rows = explode("\n", $csv);
+    $this->assertEqual(count($rows), $route_count + 1);
+  }
+}
+
 ?>
