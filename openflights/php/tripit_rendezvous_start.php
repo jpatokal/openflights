5d9c2bc26b7738761b8d232f87d954fbc1630d93
andrewachen <andrewachen@a57d767e-1c7c-476a-a379-f7d483c1f09d>
2012-03-19 00:48:50 +0000

    TripIt: Force TripIt login when linking accounts to allow user to pick an OpenID provider.

--- openflights/php/tripit_rendezvous_start.php
+++ openflights/php/tripit_rendezvous_start.php
@@ -29,5 +29,7 @@ $_SESSION["tripit_rendezvous"] = array(
   "secret" => $tokens["oauth_token_secret"]
 );
 
-header("Location: https://www.tripit.com/oauth/authorize?oauth_token=" . $tokens["oauth_token"] . "&oauth_token_secret=" . $tokens["oauth_token_secret"] . "&oauth_callback=" . urlencode("http://" . $_SERVER["SERVER_NAME"] . "/php/tripit_link.php"));
+$oauth_permission_url = "https://www.tripit.com/oauth/authorize?oauth_token=" . $tokens["oauth_token"] . "&oauth_token_secret=" . $tokens["oauth_token_secret"] . "&oauth_callback=" . urlencode("http://" . $_SERVER["SERVER_NAME"] . "/php/tripit_link.php");
+
+header("Location: https://www.tripit.com/account/login?redirect_url=" . urlencode($oauth_permission_url));
 ?>
