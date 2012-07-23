b6144f8255db2713b0dea623254b783e45573476
Jani Patokallio <jpatokal@users.noreply.github.com>
2012-07-23 10:48:36 +0000

    Correct percentages

--- sql/locales.sql
+++ sql/locales.sql
@@ -1,2 +1,2 @@
-select locale,count(*) as count,(count(*)/12513)*100 as percent from users group by locale order by count desc;
+select locale,count(*) as count,(count(*)/(select count(*) from users))*100 as percent from users group by locale order by count desc;
 
