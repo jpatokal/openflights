176b9d24216b61c5a1da0a0d1ce127b70a1b82c5
jpatokal <jpatokal@a57d767e-1c7c-476a-a379-f7d483c1f09d>
2012-08-17 12:57:11 +0000

    Refresh cache for utilities

--- openflights/index.php
+++ openflights/index.php
@@ -16,7 +16,7 @@ require_once("./php/helper.php");
     <script type="text/javascript" src="http://www.google.com/jsapi"></script>
     <script type="text/javascript" src="/OpenLayers.js?version=20091204"></script>
     <script type="text/javascript" src="/js/greatcircle.js?version=20101016"></script>
-    <script type="text/javascript" src="/js/utilities.js?version=20090326"></script>
+    <script type="text/javascript" src="/js/utilities.js?version=20120817"></script>
     <script type="text/javascript" src="/js/Gettext.js"></script>
     <script type="text/javascript" src="/js/scw.js"></script>
     <script type="text/javascript" src="/js/prototype.js?version=20090326"></script>
