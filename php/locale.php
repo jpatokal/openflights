d88dd240c39e253701404c573dfffb1482adec52
Jani Patokallio <jpatokal@users.noreply.github.com>
2013-03-12 12:19:15 +0000

    Add configuration option to disable locale support

--- php/locale.php
+++ php/locale.php
@@ -1,26 +1,35 @@
 <?php
+include 'config.php';
 session_start();
 
-if (isSet($_GET["lang"])) {
-  $locale = $_GET["lang"];
-  $_SESSION["locale"] = $locale;
+if($OF_USE_LOCALES) {
+  if (isSet($_GET["lang"])) {
+    $locale = $_GET["lang"];
+    $_SESSION["locale"] = $locale;
+  } else {
+    $locale = $_SESSION["locale"];
+    if(!$locale or empty($locale)) {
+      $locale = "en_US";
+    }
+  }
+  $locale .= ".utf8";
+  setlocale(LC_ALL, $locale);
+
+  if(substr_count($_SERVER['SCRIPT_NAME'], '/') == 1) {
+    $path = ".";
+  } else {
+    $path = "..";
+  }
+  bindtextdomain("messages", $path . "/locale");
+  textdomain("messages");
+  header("Content-type: text/html; charset=utf-8");
 } else {
-  $locale = $_SESSION["locale"];
-  if(!$locale or empty($locale)) {
-    $locale = "en_US";
+  $locale = "en_US.utf8";
+
+  function _($string) {
+    return $string;
   }
 }
-$locale .= ".utf8";
-setlocale(LC_ALL, $locale);
-
-if(substr_count($_SERVER['SCRIPT_NAME'], '/') == 1) {
-  $path = ".";
-} else {
-  $path = "..";
-}
-bindtextdomain("messages", $path . "/locale");
-textdomain("messages");
-header("Content-type: text/html; charset=utf-8");
 
 //
 // Generate select box (pulldown) with all known locales
@@ -30,13 +39,16 @@ header("Content-type: text/html; charset=utf-8");
 //
 function locale_pulldown($db, $locale) {
   echo "<select id='locale' onChange='JavaScript:changeLocale()'>\n";
-  $sql = "SELECT * FROM locales ORDER BY name ASC";
-  $result = mysql_query($sql, $db);
-  while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-    $selected = ($row["locale"] . ".utf8" == $locale ? "SELECTED" : "");
-    printf("<option value='%s' %s>%s (%s)</option>\n", $row["locale"], $selected, $row["name"], substr($row["locale"], 0, 2));
+  if($OF_USE_LOCALES) {
+    $sql = "SELECT * FROM locales ORDER BY name ASC";
+    $result = mysql_query($sql, $db);
+    while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+      $selected = ($row["locale"] . ".utf8" == $locale ? "SELECTED" : "");
+      printf("<option value='%s' %s>%s (%s)</option>\n", $row["locale"], $selected, $row["name"], substr($row["locale"], 0, 2));
+    }
+  } else {
+    echo "<option value='en_US.utf8' SELECTED>English</option>\n";
   }
   echo "</select>";
 }
-
 ?>
