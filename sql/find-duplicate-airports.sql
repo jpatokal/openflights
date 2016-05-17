0d3c6d87895a7df3598ffd81c1267b07b930ac6f
Jani Patokallio <jpatokal@iki.fi>
2016-05-17 22:24:02 +1000

    Quick helper for finding dupe airports

---
+++ sql/find-duplicate-airports.sql
@@ -0,0 +1,6 @@
+SELECT apid, name, iata, airports.icao FROM airports
+INNER JOIN (SELECT icao FROM airports 
+GROUP BY icao HAVING count(icao) > 1) dup ON airports.icao = dup.icao
+WHERE airports.icao != ""
+ORDER BY icao;
+
