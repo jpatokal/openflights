5d623a6969a1adee7961cf1c9a8a212c4a784713
Jani Patokallio <jpatokal@iki.fi>
2017-02-02 22:32:12 +1100

    Update data exports

--- sql/airports-upgrade.sql
+++ sql/airports-upgrade.sql
@@ -1,9 +1,11 @@
 ALTER TABLE airports ADD COLUMN type TEXT;
-UPDATE airports SET type='unknown';
-UPDATE airports SET type='station' WHERE name LIKE '%train %' OR name LIKE '% station' OR name LIKE '% rail%' OR name LIKE '%BF' OR name LIKE '%bahnhof%';
-UPDATE airports SET type='port' WHERE name LIKE '%ferry %' OR name LIKE '% port%';
-UPDATE airports SET type='airport' WHERE name LIKE '% air %' OR name LIKE '%airport%' OR name LIKE '%aerodrome%' OR name LIKE '%heliport%' OR name LIKE '%seaplane%';
-
 ALTER TABLE airports ADD COLUMN source TEXT;
-UPDATE airports SET source='User' WHERE uid IS NOT NULL;
-UPDATE airports SET source='Legacy' WHERE uid IS NULL;
+
+UPDATE airports SET type='unknown' IF type IS NULL;
+UPDATE airports SET type='station' WHERE (name LIKE '%train %' OR name LIKE '% station' OR name LIKE '% rail%' OR name LIKE '%BF' OR name LIKE '%bahnhof%') AND type IS NULL;
+UPDATE airports SET type='port' WHERE (name LIKE '%ferry %' OR name LIKE '% port%') AND type IS NULL;
+UPDATE airports SET type='airport' WHERE (name LIKE '% air %' OR name LIKE '%airport%' OR name LIKE '%aerodrome%' OR name LIKE '%heliport%' OR name LIKE '%seaplane%') AND type IS NULL;
+
+UPDATE airports SET source='User' WHERE source IS NULL AND uid IS NOT NULL;
+UPDATE airports SET source='Legacy' WHERE source IS NULL AND uid IS NULL;
+
