302dc5592676e86b2849edc45458d70e6ae7f632
Jani Patokallio <jpatokal@iki.fi>
2017-08-27 21:26:46 +1000

    Add special page for resetting Tripit token (#701)

---
+++ php/tripit_unlink.php
@@ -0,0 +1,22 @@
+<?php
+require_once("locale.php");
+require_once("db_pdo.php");
+require_once("tripit_common.php");
+
+$uid = $_SESSION["uid"];
+if (!$uid or empty($uid)) {
+  print _("Session expired.  Please try re-linking after logging back in.");
+  exit();
+}
+
+try {
+  $sth = $dbh->prepare("update tripit_tokens set active='N' where uid=?");
+  $sth->execute(array($uid));
+} catch (PDOException $e) {
+  die(_("Failed to disable old TripIt links."));
+}
+
+// All good, redirect back to listing
+header("Location: /php/tripit_list_trips.php");
+
+?>
