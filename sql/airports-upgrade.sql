5dd45e60298e7bd5d788abb9e842fbd97116fa3d
Jani Patokallio <jpatokal@iki.fi>
2017-11-23 22:06:14 +1100

    Fix typo

--- sql/airports-upgrade.sql
+++ sql/airports-upgrade.sql
@@ -1,7 +1,7 @@
 ALTER TABLE airports ADD COLUMN type TEXT;
 ALTER TABLE airports ADD COLUMN source TEXT;
 
-UPDATE airports SET type='unknown' IF type IS NULL;
+UPDATE airports SET type='unknown' WHERE type IS NULL;
 UPDATE airports SET type='station' WHERE (name LIKE '%train %' OR name LIKE '% station' OR name LIKE '% rail%' OR name LIKE '%BF' OR name LIKE '%bahnhof%') AND type IS NULL;
 UPDATE airports SET type='port' WHERE (name LIKE '%ferry %' OR name LIKE '% port%') AND type IS NULL;
 UPDATE airports SET type='airport' WHERE (name LIKE '% air %' OR name LIKE '%airport%' OR name LIKE '%aerodrome%' OR name LIKE '%heliport%' OR name LIKE '%seaplane%') AND type IS NULL;
