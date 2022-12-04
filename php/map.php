f4069b5ca7fa68d0550316a3d4a025b69bf4112c
Jani Patokallio <jpatokal@iki.fi>
2022-12-04 06:49:26 +0000

    Fix silly logic bug for distance units (#1111)

--- php/map.php
+++ php/map.php
@@ -128,7 +128,7 @@ if($row = $sth->fetch()) {
   }
   $distance = $row["distance"];
   if(!$distance) $distance = "0";
-  if($_SESSION["units"] ?? null == "K") {
+  if($_SESSION["units"] == "K") {
     $distance = round($distance * $KMPERMILE) . " " . _("km");
   } else {
     $distance = $distance . " " . _("miles");
