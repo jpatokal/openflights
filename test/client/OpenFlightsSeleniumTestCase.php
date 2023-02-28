260b9736230e27212761561ce119cd231c86157a
Sam Reed <sam@reedyboy.net>
2023-02-28 16:57:06 +0000

    Fixing Selenium tests...

--- test/client/OpenFlightsSeleniumTestCase.php
+++ test/client/OpenFlightsSeleniumTestCase.php
@@ -1,15 +1,18 @@
 <?php
+
+use PHPUnit\Extensions\Selenium2TestCase;
+
 require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
 
 include_once(dirname(__FILE__) . '/../server/config.php');
 
-class OpenFlightsSeleniumTestCase extends PHPUnit_Extensions_SeleniumTestCase
+class OpenFlightsSeleniumTestCase extends Selenium2TestCase
 {
   protected $captureScreenshotOnFailure = true;
   protected $screenshotPath = '/tmp/screenshots';
   protected $screenshotUrl = '/tmp/screenshots';
 
-  protected function setUp()
+  protected function setUp(): void
   {
     $this->setBrowser('*firefox /Applications/Firefox.app/Contents/MacOS/firefox-bin');
     $this->setBrowserUrl('http://openflights.local/');
