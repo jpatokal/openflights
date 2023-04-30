bf51530e6ee862ceda3cf8c57e2bf772533ec616
Reedy <reedy@wikimedia.org>
2023-04-30 16:57:11 +0100

    map-routes.php: Cleanup

--- data/map-routes.php
+++ data/map-routes.php
@@ -7,9 +7,9 @@ $stderr = fopen("php://stderr", "w");
 
 function getlocationcoords($lat, $lon, $width, $height)
 {
-   $x = (($lon + 180) * ($width / 360));
-   $y = ((($lat * -1) + 90) * ($height / 180));
-   return array("x"=>round($x),"y"=>round($y));
+    $x = (($lon + 180) * ($width / 360));
+    $y = ((($lat * -1) + 90) * ($height / 180));
+    return array("x" => round($x),"y" => round($y));
 }
 
 $sql = "SELECT DISTINCT s.x AS sx,s.y AS sy,d.x AS dx,d.y AS dy FROM routes AS r, airports AS s, airports AS d WHERE r.src_apid=s.apid AND r.dst_apid=d.apid GROUP BY s.apid,d.apid";
@@ -20,19 +20,21 @@ $sql = "SELECT DISTINCT s.x AS sx,s.y AS sy,d.x AS dx,d.y AS dy FROM routes AS r
 
 //$im = imagecreatefrompng("metacarta_wmsc.png");
 $im = imagecreatefrompng("map-2048.png");
-if(! $im) {
-  die("Image not found");
+if (! $im) {
+    die("Image not found");
 }
 
 imagealphablending($im, true);
-$airportColors=array(imagecolorallocate ($im, 0,0,0), // black
-		     imagecolorallocate ($im, 0x66,0x66,0x99), // cyan
-		     imagecolorallocate ($im, 0x45,0xFF,0xA9)); // green
+$airportColors = array(
+    imagecolorallocate($im, 0,0,0), // black
+    imagecolorallocate($im, 0x66,0x66,0x99), // cyan
+    imagecolorallocate($im, 0x45,0xFF,0xA9) // green
+);
 
-$yellow = imagecolorallocatealpha ($im, 0x99, 0xEE, 0, 95);
+$yellow = imagecolorallocatealpha($im, 0x99, 0xEE, 0, 95);
 
 // Next need to find the base image size.
-// We need these variables to be able scale the long/lat coordinates.
+// We need these variables to be able to scale the long/lat coordinates.
 
 $scale_x = imagesx($im);
 $scale_y = imagesy($im);
@@ -41,43 +43,52 @@ $scale_y = imagesy($im);
 
 $count = 0;
 foreach ($dbh->query($sql) as $row) {
-  $count++;
-  if($count % 100 == 0) fwrite($stderr, "$count ");
-
-  $x1 = $row["sx"];
-  $y1 = $row["sy"];
-  $x2 = $row["dx"];
-  $y2 = $row["dy"];
-
-  $distance = gcPointDistance(array("x" => $x1, "y" => $y1),
-			      array("x" => $x2, "y" => $y2));
-
-  if($distance > GC_MIN) {
-    // Plot great circle curve
-    $points = gcPath(array("x" => $x1, "y" => $y1),
-		     array("x" => $x2, "y" => $y2),
-		     $distance, false);
-  } else {
-    // Draw straight lines
-    $points = straightPath(array("x" => $x1, "y" => $y1),
-			   array("x" => $x2, "y" => $y2));
-  }
-  $oldpt = null;
-  foreach($points as $loc) {
-    if($loc == null) {
-      $oldpt = null;
-      continue;
+    $count++;
+    if ($count % 100 == 0) {
+        fwrite($stderr, "$count ");
     }
-    $newpt = getlocationcoords($loc["y"], $loc["x"], $scale_x, $scale_y);
-    if($oldpt) {
-      imageline($im, $oldpt["x"], $oldpt["y"], $newpt["x"], $newpt["y"], $yellow);
+
+    $x1 = $row["sx"];
+    $y1 = $row["sy"];
+    $x2 = $row["dx"];
+    $y2 = $row["dy"];
+
+    $distance = gcPointDistance(
+        array("x" => $x1, "y" => $y1),
+        array("x" => $x2, "y" => $y2)
+    );
+
+    if ($distance > GC_MIN) {
+        // Plot great circle curve
+        $points = gcPath(
+            array("x" => $x1, "y" => $y1),
+            array("x" => $x2, "y" => $y2),
+            $distance,
+            false
+        );
+    } else {
+        // Draw straight lines
+        $points = straightPath(
+            array("x" => $x1, "y" => $y1),
+            array("x" => $x2, "y" => $y2)
+        );
     }
-    $oldpt = $newpt;
-  }
+    $oldpt = null;
+    foreach ($points as $loc) {
+        if ($loc == null) {
+            $oldpt = null;
+            continue;
+        }
+        $newpt = getlocationcoords($loc["y"], $loc["x"], $scale_x, $scale_y);
+        if ($oldpt) {
+            imageline($im, $oldpt["x"], $oldpt["y"], $newpt["x"], $newpt["y"], $yellow);
+        }
+        $oldpt = $newpt;
+    }
 }
 
 // Return the map image. We are using a PNG format as it gives better final image quality than a JPG
 
-header ("Content-type: image/png");
+header("Content-type: image/png");
 imagepng($im);
 imagedestroy($im);
