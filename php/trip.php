beff959752d0148655af771b130052641fcb0a72
Reedy <reedy@wikimedia.org>
2023-04-30 13:57:40 +0100

    trip.php: Cleanup

--- php/trip.php
+++ php/trip.php
@@ -1,4 +1,5 @@
 <?php
+
 include 'locale.php';
 include 'db_pdo.php';
 
@@ -8,58 +9,61 @@ $url = $_POST["url"];
 $trid = $_POST["trid"];
 $privacy = $_POST["privacy"];
 
-if($type != "NEW" and (!$trid or $trid == 0)) {
-  die ('0;Trip ID '. $trid . ' invalid');
+if ($type != "NEW" && (!$trid or $trid == 0)) {
+    die('0;Trip ID ' . $trid . ' invalid');
 }
 
 $uid = $_SESSION["uid"];
-if(!$uid or empty($uid)) {
-  die ('0;' . _("Your session has timed out, please log in again."));
-  exit;
+if (!$uid || empty($uid)) {
+    die('0;' . _("Your session has timed out, please log in again."));
 }
 
-switch($type) {
- case "NEW":
-   // Create new trip
-   $sth = $dbh->prepare("INSERT INTO trips(name,url,public,uid) VALUES(?,?,?,?)");
-   $success = $sth->execute([$name, $url, $privacy, $uid]);
-   break;
+switch ($type) {
+    case "NEW":
+        // Create new trip
+        $sth = $dbh->prepare("INSERT INTO trips(name,url,public,uid) VALUES(?,?,?,?)");
+        $success = $sth->execute([$name, $url, $privacy, $uid]);
+        break;
 
- case "EDIT":
-   // Edit existing trip
-   $sth = $dbh->prepare("UPDATE trips SET name=?, url=?, public=? WHERE uid=? AND trid=?");
-   $success = $sth->execute([$name, $url, $privacy, $uid, $trid]);
-   break;
+    case "EDIT":
+        // Edit existing trip
+        $sth = $dbh->prepare("UPDATE trips SET name=?, url=?, public=? WHERE uid=? AND trid=?");
+        $success = $sth->execute([$name, $url, $privacy, $uid, $trid]);
+        break;
 
-   // Assign its flights to null and delete trip
- case "DELETE":
-   $sth = $dbh->prepare("UPDATE flights SET trid=NULL WHERE trid=? AND uid=?");
-   $sth->execute([$trid, $uid]) or die ('0;Operation on trip ' . $name . ' failed.');
+    case "DELETE":
+        // Assign its flights to null and delete trip
+        $sth = $dbh->prepare("UPDATE flights SET trid=NULL WHERE trid=? AND uid=?");
+        if (!$sth->execute([$trid, $uid])) {
+            die('0;Operation on trip ' . $name . ' failed.');
+        }
 
-   $sth = $dbh->prepare("DELETE FROM trips WHERE trid=? AND uid=?");
-   $success = $sth->execute([$trid, $uid]);
-   break;
+        $sth = $dbh->prepare("DELETE FROM trips WHERE trid=? AND uid=?");
+        $success = $sth->execute([$trid, $uid]);
+        break;
 
- default:
-   die ('0;Unknown operation ' . $type);
+    default:
+        die('0;Unknown operation ' . $type);
 }
 
-$success or die ('0;Operation on trip ' . $name . ' failed.');
-if($sth->rowCount() != 1) {
-  die("0;No matching trip found");
+if (!$success) {
+    die('0;Operation on trip ' . $name . ' failed.');
+}
+if ($sth->rowCount() != 1) {
+    die("0;No matching trip found");
 }
 
-switch($type) {
- case "NEW":
-   $trid = $dbh->lastInsertId();
-   printf("1;%s;" . _("Trip successfully created"), $trid);
-   break;
+switch ($type) {
+    case "NEW":
+        $trid = $dbh->lastInsertId();
+        printf("1;%s;" . _("Trip successfully created"), $trid);
+        break;
 
- case "DELETE":
-   printf("100;%s;" . _("Trip successfully deleted"), $trid);
-   break;
+    case "DELETE":
+        printf("100;%s;" . _("Trip successfully deleted"), $trid);
+        break;
 
- default:
-   printf("2;%s;" . _("Trip successfully edited."), $trid);
-   break;
+    default:
+        printf("2;%s;" . _("Trip successfully edited."), $trid);
+        break;
 }
