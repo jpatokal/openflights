b5dad01c3092259ac8ca960e243d574e052cc164
Reedy <reedy@wikimedia.org>
2023-04-30 13:03:24 +0100

    banner.php: Update

--- badge/banner.php
+++ badge/banner.php
@@ -4,84 +4,97 @@ include '../php/helper.php';
 
 putenv('GDFONTPATH=' . realpath('.'));
 
-function endsWith( $str, $sub ) {
-  return ( substr( $str, strlen( $str ) - strlen( $sub ) ) == $sub );
+function renderError($string) {
+    renderImage(array(
+        37 => "Error:",
+        52 => $string,
+    ));
+    exit(0);
 }
 
-function rendererror($string) {
-  $font = "OpenSans-Regular";
-  $im = imagecreatefrompng("banner.png");
-  $white = imagecolorallocate ($im, 0xFF, 0xFF, 0xFF);
-  imagettftext($im, 10.5, 0, 220, 37, $white, $font, "Error:");
-  imagettftext($im, 10.5, 0, 220, 52, $white, $font, $string);
-  imagepng($im);
-  imagedestroy($im);
-  exit(0);
+/**
+ * @param $textLines array [ $yIndex => $text ]
+ * @param $user string|false Username
+ */
+function renderImage($textLines, $user = false) {
+    $im = imagecreatefrompng("banner.png");
+    $white = imagecolorallocate($im, 0xFF, 0xFF, 0xFF);
+    foreach ($textLines as $y => $text) {
+        imagettftext($im, 10.5, 0, 220, $y, $white, "OpenSans-Regular", $text);
+    }
+    // Only write to the cache if we have a user (ie, not in error cases)
+    if ($user) {
+        imagepng($im, 'cache/' . $user);
+    }
+    imagepng($im);
+    imagedestroy($im);
 }
 
-header ("Content-type: image/png");
+header("Content-type: image/png");
 $user = $_GET["user"];
-if(! $user || $user == "") {
-  rendererror("User name missing");
+if (!$user || $user == "") {
+    renderError("User name missing");
 }
 
 // Trim off any trailing ".png"
-if(endsWith($user, ".png")) {
-  $user = substr($user, 0, strlen($user)-strlen(".png"));
+if (strpos($user, ".png") !== false) {
+    $user = pathinfo($user, PATHINFO_FILENAME);
 }
 
 // Do we have a recent (< 1 hr) cached copy?
 $cache = "cache/" . $user;
-if(file_exists($cache) && (time() - filemtime($cache) < 3600)) {
-  $im = imagecreatefrompng($cache);
-  imagepng($im);
-  imagedestroy($im);
-  return;
+if (file_exists($cache) && (time() - filemtime($cache) < 3600)) {
+    $im = imagecreatefrompng($cache);
+    imagepng($im);
+    imagedestroy($im);
+    return;
 }
 
 // New banner or cache out of date, so regenerate
 $sth = $dbh->prepare("SELECT uid, public, units FROM users WHERE name=?");
 $result = $sth->execute([$user]);
-if(! $result) {
-  rendererror("Database error 1");
+if (!$result) {
+    renderError("Database error 1");
 }
-if($sth->rowCount() == 0) {
-  rendererror("User $user not found");
+if ($sth->rowCount() == 0) {
+    renderError("User $user not found");
 }
 $row = $sth->fetch();
-if($row["public"] == "N") {
-  rendererror("User is not public");
+if ($row["public"] == "N") {
+    renderError("User is not public");
 }
 $uid = $row["uid"];
 $units = $row["units"];
 
-$sth = $dbh->prepare("SELECT COUNT(*) AS count, SUM(distance) AS distance, SUM(TIME_TO_SEC(duration))/60 AS duration FROM flights WHERE uid=?");
+$sth = $dbh->prepare(
+    "SELECT COUNT(*) AS count, SUM(distance) AS distance, SUM(TIME_TO_SEC(duration))/60 AS duration FROM flights WHERE uid=?"
+);
 $result = $sth->execute([$uid]);
-if($result && $row = $sth->fetch()) {
-  $distance = $row["distance"];
-  if($units == "K") {
-    $distance *= $KMPERMILE;
-    $units = "km";
-  } else {
-    $units = "miles";
-  }
-  $flights = sprintf("%s flights", $row["count"]);
-  $miles = sprintf("%d,%03d %s", $distance / 1000, $distance % 1000, $units);
-  $duration = sprintf("%d days, %d:%02d hours", $row["duration"] / 1440, ($row["duration"] / 60) % 24, $row["duration"] % 60);
+if ($result && $row = $sth->fetch()) {
+    $distance = $row["distance"];
+    if ($units == "K") {
+        $distance *= $KMPERMILE;
+        $units = "km";
+    } else {
+        $units = "miles";
+    }
+    $flights = sprintf("%s flights", $row["count"]);
+    $miles = sprintf("%d,%03d %s", $distance / 1000, $distance % 1000, $units);
+    $duration = sprintf(
+        "%d days, %d:%02d hours",
+        $row["duration"] / 1440,
+        ($row["duration"] / 60) % 24,
+        $row["duration"] % 60
+    );
 } else {
-  rendererror("Database error 2");
+    renderError("Database error 2");
 }
 
-$font = "OpenSans-Regular";
-$im = imagecreatefrompng("banner.png");
-$white = imagecolorallocate ($im, 0xFF, 0xFF, 0xFF);
-imagettftext($im, 10.5, 0, 220, 37, $white, $font, $flights);
-imagettftext($im, 10.5, 0, 220, 52, $white, $font, $miles);
-imagettftext($im, 10.5, 0, 220, 65, $white, $font, $duration);
-
-// Write a copy to the cache
-imagepng($im, 'cache/' . $user);
-
-// And output to the browser
-imagepng($im);
-imagedestroy($im);
+renderImage(
+    array(
+        37 => $flights,
+        52 => $miles,
+        65 => $duration,
+    ),
+    $user
+);
