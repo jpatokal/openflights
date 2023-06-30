f123f9473bbf579b30f30220e58209cfdd92fb6d
Christopher Rosset <chris@rosset.tech>
2023-06-30 18:09:15 -0400

    Return JSON-encoded results from `top10.php`
    `top10.php` now returns JSON for both the data and errors. This change
    also includes the front-end side which consumes the new format.
    
    A baby step towards https://github.com/jpatokal/openflights/issues/876.

--- test/server/top10.php
+++ test/server/top10.php
@@ -9,6 +9,16 @@ include_once(dirname(__FILE__) . '/config.php');
 
 $fid = null; // global for newly-added flight
 
+// Return first value matching $pred or null if no matches
+function array_find($array, $pred) {
+    foreach ($array as $value) {
+        if ($pred($value)) {
+            return $value;
+        }
+    }
+    return null;
+}
+
 // Check default (flight count) Top 10 stats
 class CheckTop10FlightCountStats extends WebTestCase {
     public function test() {
@@ -17,26 +27,50 @@ class CheckTop10FlightCountStats extends WebTestCase {
         assert_login($this);
 
         $top10 = $this->post($webroot . "php/top10.php");
-        $rows = preg_split('/\n/', $top10);
-        $this->assertTrue(sizeof($rows) == 4);
+        $json = json_decode($top10, true);
+
+        $this->assertTrue(array_key_exists("routes", $json));
+        $this->assertTrue(array_key_exists("airlines", $json));
+        $this->assertTrue(array_key_exists("airports", $json));
+        $this->assertTrue(array_key_exists("planes", $json));
 
         // Top 10 routes
-        $routes = $rows[0];
-        $this->assertPattern("/," . $flight2["src_apid"] . ",/", $routes);
-        $this->assertPattern("/," . $flight2["dst_apid"] . ",1/", $routes);
+        $route_src = array_find($json["routes"], function($val) use ($flight2) {
+            return $val["src_apid"] == $flight2["src_apid"];
+        });
+        $this->assertNotEqual($route_src, null);
+        $this->assertEqual($route_src["count"], 1);
+        $route_dst = array_find($json["routes"], function($val) use ($flight2) {
+            return $val["dst_apid"] == $flight2["dst_apid"];
+        });
+        $this->assertNotEqual($route_dst, null);
+        $this->assertEqual($route_dst["count"], 1);
 
         // Top 10 airports
-        $airports = $rows[1];
-        $this->assertPattern("/,1," . $flight2["src_apid"] . "/", $airports);
-        $this->assertPattern("/,1," . $flight2["dst_apid"] . "/", $airports);
+        $airport_src = array_find($json["airports"], function($val) use ($flight2) {
+            return $val["apid"] == $flight2["src_apid"];
+        });
+        $this->assertNotEqual($airport_src, null);
+        $this->assertEqual($airport_src["count"], 1);
+        $airport_dst = array_find($json["airports"], function($val) use ($flight2) {
+            return $val["apid"] == $flight2["dst_apid"];
+        });
+        $this->assertNotEqual($airport_dst, null);
+        $this->assertEqual($airport_dst["count"], 1);
 
         // Top 10 airlines
-        $airlines = $rows[2];
-        $this->assertPattern("/,1," . $flight2["alid"] . "/", $airlines);
+        $airline = array_find($json["airlines"], function($val) use ($flight2) {
+            return $val["alid"] == $flight2["alid"];
+        });
+        $this->assertNotEqual($airline, null);
+        $this->assertEqual($airline["count"], 1);
 
         // Top 10 planes
-        $planes = $rows[3];
-        $this->assertPattern("/" . $flight2["plane"] . ",1/", $planes);
+        $plane = array_find($json["planes"], function($val) use ($flight2) {
+            return $val["name"] == $flight2["plane"];
+        });
+        $this->assertNotEqual($plane, null);
+        $this->assertEqual($plane["count"], 1);
     }
 }
 
@@ -48,27 +82,52 @@ class CheckTop10AirlineFilteredFlightCountStats extends WebTestCase {
         assert_login($this);
 
         $filter = array("alid" => $flight2["alid"]);
+
         $top10 = $this->post($webroot . "php/top10.php", $filter);
-        $rows = preg_split('/\n/', $top10);
-        $this->assertTrue(sizeof($rows) == 4);
+        $json = json_decode($top10, true);
+
+        $this->assertTrue(array_key_exists("routes", $json));
+        $this->assertTrue(array_key_exists("airlines", $json));
+        $this->assertTrue(array_key_exists("airports", $json));
+        $this->assertTrue(array_key_exists("planes", $json));
 
         // Top 10 routes
-        $routes = $rows[0];
-        $this->assertPattern("/," . $flight2["src_apid"] . ",/", $routes);
-        $this->assertPattern("/," . $flight2["dst_apid"] . ",1/", $routes);
+        $route_src = array_find($json["routes"], function($val) use ($flight2) {
+            return $val["src_apid"] == $flight2["src_apid"];
+        });
+        $this->assertNotEqual($route_src, null);
+        $this->assertEqual($route_src["count"], 1);
+        $route_dst = array_find($json["routes"], function($val) use ($flight2) {
+            return $val["dst_apid"] == $flight2["dst_apid"];
+        });
+        $this->assertNotEqual($route_dst, null);
+        $this->assertEqual($route_dst["count"], 1);
 
         // Top 10 airports
-        $airports = $rows[1];
-        $this->assertPattern("/,1," . $flight2["src_apid"] . "/", $airports);
-        $this->assertPattern("/,1," . $flight2["dst_apid"] . "/", $airports);
+        $airport_src = array_find($json["airports"], function($val) use ($flight2) {
+            return $val["apid"] == $flight2["src_apid"];
+        });
+        $this->assertNotEqual($airport_src, null);
+        $this->assertEqual($airport_src["count"], 1);
+        $airport_dst = array_find($json["airports"], function($val) use ($flight2) {
+            return $val["apid"] == $flight2["dst_apid"];
+        });
+        $this->assertNotEqual($airport_dst, null);
+        $this->assertEqual($airport_dst["count"], 1);
 
         // Top 10 airlines
-        $airlines = $rows[2];
-        $this->assertPattern("/,1," . $flight2["alid"] . "/", $airlines);
+        $airline = array_find($json["airlines"], function($val) use ($flight2) {
+            return $val["alid"] == $flight2["alid"];
+        });
+        $this->assertNotEqual($airline, null);
+        $this->assertEqual($airline["count"], 1);
 
         // Top 10 planes
-        $planes = $rows[3];
-        $this->assertPattern("/" . $flight2["plane"] . ",1/", $planes);
+        $plane = array_find($json["planes"], function($val) use ($flight2) {
+            return $val["name"] == $flight2["plane"];
+        });
+        $this->assertNotEqual($plane, null);
+        $this->assertEqual($plane["count"], 1);
     }
 }
 
@@ -81,27 +140,52 @@ class CheckTop10DistanceStats extends WebTestCase {
 
         $params = array("mode" => "D");
         $top10 = $this->post($webroot . "php/top10.php", $params);
-        $rows = preg_split('/\n/', $top10);
-        $this->assertTrue(sizeof($rows) == 4);
 
         $distance = $flight2["distance"];
 
+        $json = json_decode($top10, true);
+
+        $this->assertTrue(array_key_exists("routes", $json));
+        $this->assertTrue(array_key_exists("airlines", $json));
+        $this->assertTrue(array_key_exists("airports", $json));
+        $this->assertTrue(array_key_exists("planes", $json));
+
         // Top 10 routes
-        $routes = $rows[0];
-        $this->assertPattern("/," . $flight2["src_apid"] . ",/", $routes);
-        $this->assertPattern("/," . $flight2["dst_apid"] . ",$distance/", $routes);
+        $route_src = array_find($json["routes"], function($val) use ($flight2) {
+            return $val["src_apid"] == $flight2["src_apid"];
+        });
+        $this->assertNotEqual($route_src, null);
+        $this->assertEqual($route_src["count"], $distance);
+        $route_dst = array_find($json["routes"], function($val) use ($flight2) {
+            return $val["dst_apid"] == $flight2["dst_apid"];
+        });
+        $this->assertNotEqual($route_dst, null);
+        $this->assertEqual($route_dst["count"], $distance);
 
         // Top 10 airports
-        $airports = $rows[1];
-        $this->assertPattern("/,$distance," . $flight2["src_apid"] . "/", $airports);
-        $this->assertPattern("/,$distance," . $flight2["dst_apid"] . "/", $airports);
+        $airport_src = array_find($json["airports"], function($val) use ($flight2) {
+            return $val["apid"] == $flight2["src_apid"];
+        });
+        $this->assertNotEqual($airport_src, null);
+        $this->assertEqual($airport_src["count"], $distance);
+        $airport_dst = array_find($json["airports"], function($val) use ($flight2) {
+            return $val["apid"] == $flight2["dst_apid"];
+        });
+        $this->assertNotEqual($airport_dst, null);
+        $this->assertEqual($airport_dst["count"], $distance);
 
         // Top 10 airlines
-        $airlines = $rows[2];
-        $this->assertPattern("/,$distance," . $flight2["alid"] . "/", $airlines);
+        $airline = array_find($json["airlines"], function($val) use ($flight2) {
+            return $val["alid"] == $flight2["alid"];
+        });
+        $this->assertNotEqual($airline, null);
+        $this->assertEqual($airline["count"], $distance);
 
         // Top 10 planes
-        $planes = $rows[3];
-        $this->assertPattern("/" . $flight2["plane"] . ",$distance/", $planes);
+        $plane = array_find($json["planes"], function($val) use ($flight2) {
+            return $val["name"] == $flight2["plane"];
+        });
+        $this->assertNotEqual($plane, null);
+        $this->assertEqual($plane["count"], $distance);
     }
 }
