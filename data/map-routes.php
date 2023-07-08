6b17761a83d1ac819c693e050365b060291b0ab7
Sam Reed <sam@reedyboy.net>
2023-07-08 12:56:44 +0100

    map-routes.php: Minor cleanup

--- data/map-routes.php
+++ data/map-routes.php
@@ -3,24 +3,10 @@
 include("../php/db_pdo.php");
 include("../php/greatcircle.php");
 
-$stderr = fopen("php://stderr", "w");
-
-function getlocationcoords($lat, $lon, $width, $height)
-{
-    $x = (($lon + 180) * ($width / 360));
-    $y = ((($lat * -1) + 90) * ($height / 180));
-    return array("x" => round($x),"y" => round($y));
-}
-
-$sql = "SELECT DISTINCT s.x AS sx,s.y AS sy,d.x AS dx,d.y AS dy FROM routes AS r, airports AS s, airports AS d WHERE r.src_apid=s.apid AND r.dst_apid=d.apid GROUP BY s.apid,d.apid";
-
 // First we load the background/base map. We assume it's located in same dir as the script.
-// This can be any format but we are using JPG in this example
-// We will also allocate the color for the marker
-
-//$im = imagecreatefrompng("metacarta_wmsc.png");
+// This can be any format. We will also allocate the color for the marker.
 $im = imagecreatefrompng("map-2048.png");
-if (! $im) {
+if (!$im) {
     die("Image not found");
 }
 
@@ -35,60 +21,60 @@ $yellow = imagecolorallocatealpha($im, 0x99, 0xEE, 0, 95);
 
 // Next need to find the base image size.
 // We need these variables to be able to scale the long/lat coordinates.
-
 $scale_x = imagesx($im);
 $scale_y = imagesy($im);
 
-// Now we convert the long/lat coordinates into screen coordinates
+$stderr = fopen("php://stderr", "w");
 
+/**
+ * @param $lat
+ * @param $lon
+ * @param $width
+ * @param $height
+ * @return array
+ */
+function getlocationcoords($lat, $lon, $width, $height) {
+    $x = (($lon + 180) * ($width / 360));
+    $y = ((($lat * -1) + 90) * ($height / 180));
+    return array("x" => round($x),"y" => round($y));
+}
+
+$sql = "SELECT DISTINCT s.x AS sx,s.y AS sy,d.x AS dx,d.y AS dy FROM routes AS r, airports AS s, airports AS d WHERE r.src_apid=s.apid AND r.dst_apid=d.apid GROUP BY s.apid,d.apid";
+
+// Now we convert the long/lat coordinates into screen coordinates
 $count = 0;
 foreach ($dbh->query($sql) as $row) {
     $count++;
-    if ($count % 100 == 0) {
+    if ($count % 100 === 0) {
         fwrite($stderr, "$count ");
     }
 
-    $x1 = $row["sx"];
-    $y1 = $row["sy"];
-    $x2 = $row["dx"];
-    $y2 = $row["dy"];
-
-    $distance = gcPointDistance(
-        array("x" => $x1, "y" => $y1),
-        array("x" => $x2, "y" => $y2)
-    );
+    $from = array("x" => $row["sx"], "y" => $row["sy"]);
+    $to = array("x" => $row["dx"], "y" => $row["dy"]);
+    $distance = gcPointDistance($from, $to);
 
     if ($distance > GC_MIN) {
         // Plot great circle curve
-        $points = gcPath(
-            array("x" => $x1, "y" => $y1),
-            array("x" => $x2, "y" => $y2),
-            $distance,
-            false
-        );
+        $points = gcPath($from, $to, $distance, false);
     } else {
         // Draw straight lines
-        $points = straightPath(
-            array("x" => $x1, "y" => $y1),
-            array("x" => $x2, "y" => $y2)
-        );
+        $points = straightPath($from, $to);
     }
-    $oldpt = null;
+    $oldPoint = null;
     foreach ($points as $loc) {
-        if ($loc == null) {
-            $oldpt = null;
+        if ($loc === null) {
+            $oldPoint = null;
             continue;
         }
-        $newpt = getlocationcoords($loc["y"], $loc["x"], $scale_x, $scale_y);
-        if ($oldpt) {
-            imageline($im, $oldpt["x"], $oldpt["y"], $newpt["x"], $newpt["y"], $yellow);
+        $newPoint = getlocationcoords($loc["y"], $loc["x"], $scale_x, $scale_y);
+        if ($oldPoint) {
+            imageline($im, $oldPoint["x"], $oldPoint["y"], $newPoint["x"], $newPoint["y"], $yellow);
         }
-        $oldpt = $newpt;
+        $oldPoint = $newPoint;
     }
 }
 
 // Return the map image. We are using a PNG format as it gives better final image quality than a JPG
-
 header("Content-type: image/png");
 imagepng($im);
 imagedestroy($im);
