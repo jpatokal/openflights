a6698d397e3c8b01220eae24ba1d82a9e89653bf
Sam Reed <sam@reedyboy.net>
2023-07-11 21:22:49 +0000

    Display User ID on settings
    Fixes https://github.com/jpatokal/openflights/issues/1295

--- html/settings.php
+++ html/settings.php
@@ -51,27 +51,27 @@ if ($type == "signup") {
     );
     ?>
           <tr>
-                <td colspan="3"><h2><?php echo _("Basic information") ?></h2></td>
+                <td colspan="3"><h2><?php echo _("Basic information"); ?></h2></td>
           </tr>
           <tr>
-            <td class="key"><?php echo _("Username") ?></td>
+            <td class="key"><?php echo _("Username"); ?></td>
             <td class="value"><input type="text" name="username" size="20" onChange="changeName();"></td>
-            <td class="value"><p><?php echo _("This will be used as the name of your profile.") ?></p>
+            <td class="value"><p><?php echo _("This will be used as the name of your profile."); ?></p>
               <span id="profileurl"></span></td>
           </tr>
           <tr>
-              <td class="key"><?php echo _("Password") ?></td>
+              <td class="key"><?php echo _("Password"); ?></td>
               <td class="value"><input type="password" name="pw1" size="20"></td>
               <td class="value"><?php
-                  echo _("Pick something hard to guess, but easy to remember. Case-sensitive!")?>
+                  echo _("Pick something hard to guess, but easy to remember. Case-sensitive!"); ?>
               </td>
           </tr>
           <tr>
-              <td class="key"><?php echo _("Password again") ?></td>
+              <td class="key"><?php echo _("Password again"); ?></td>
               <td class="value"><input type="password" name="pw2" size="20"></td>
           </tr>
           <tr>
-              <td class="key"><?php echo _("E-mail (optional)") ?>&nbsp;&nbsp;</td>
+              <td class="key"><?php echo _("E-mail (optional)"); ?>&nbsp;&nbsp;</td>
               <td class="value"><input type="text" name="email" size="20"></td>
               <td class="desc">
               <?php printf(
@@ -98,23 +98,27 @@ if ($type == "signup") {
     }
     ?>
           <tr>
-            <td class="key"><nobr><?php echo _("Profile address") ?></nobr></td>
+              <td class="key"><?php echo _("User ID"); ?></td>
+              <td class="value"><?php echo $uid; ?></td>
+          </tr>
+          <tr>
+            <td class="key"><nobr><?php echo _("Profile address"); ?></nobr></td>
             <td class="value"><input type="text" name="myurl" value="<?php
-                echo "https://openflights.org/user/" . $settings["name"]
+                echo "https://openflights.org/user/" . $settings["name"];
             ?>" style="border:none" size="40" READONLY>
                 <input type="text" name="count" value="<?php
-                    printf(_("Viewed %s times"), $settings["count"]) ?>" style="border: none" READONLY>
+                    printf(_("Viewed %s times"), $settings["count"]); ?>" style="border: none" READONLY>
             </td>
             <td class="desc"><?php
-                echo _("The public address of your profile and how often it has been viewed.") ?>
+                echo _("The public address of your profile and how often it has been viewed."); ?>
             </td>
             <td class="value" rowspan=3><span id="eliteicon"></span>
-                <input type="hidden" name="elite" value="<?php echo $settings["elite"] ?>">
-                <input type="hidden" name="validity" value="<?php echo $settings["validity"] ?>">
+                <input type="hidden" name="elite" value="<?php echo $settings["elite"]; ?>">
+                <input type="hidden" name="validity" value="<?php echo $settings["validity"]; ?>">
             </td>
           </tr>
           <tr>
-              <td class="key"><?php echo _("Banners") ?></td>
+              <td class="key"><?php echo _("Banners"); ?></td>
               <td class="value" colspan=2><?php echo _("Blog banner (HTML)") ?><br>
                   <textarea name="banner_html" cols="60" rows="4" readonly><?php
                     echo "<a href='https://openflights.org/user/" .
@@ -122,7 +126,7 @@ if ($type == "signup") {
                         "' target='_blank'><img src='https://openflights.org/banner/" .
                         $settings["name"] . ".png' width=400 height=70></a>"; ?>
                   </textarea><br>
-           <?php echo _("Bulletin board banner (phpBB)") ?><br>
+           <?php echo _("Bulletin board banner (phpBB)"); ?><br>
               <textarea name="banner_phpbb" cols="60" rows="3" readonly><?php
                 echo "[url=https://openflights.org/user/" .
                     $settings["name"] .
@@ -131,13 +135,13 @@ if ($type == "signup") {
                     ".png[/img][/url]";
                 ?></textarea><br>
               <span id="banner_img"><?php
-                  echo "<img src='/banner/" . $settings["name"] . ".png' width=400 height=70>"?>
+                  echo "<img src='/banner/" . $settings["name"] . ".png' width=400 height=70>"; ?>
               </span>
               </td>
           </tr>
           <tr>
-            <td class="key"><?php echo _("E-mail (optional)") ?>&nbsp;&nbsp;</td>
-            <td class="value"><input type="text" name="email" value="<?php echo $settings["email"] ?>" size="20" />
+            <td class="key"><?php echo _("E-mail (optional)"); ?>&nbsp;&nbsp;</td>
+            <td class="value"><input type="text" name="email" value="<?php echo $settings["email"]; ?>" size="20" />
             <td class="desc">
               <?php printf(
                 _("If you forget your password, we can mail you a new one to this address. We will <i>never</i> send you any other mail or share your private information, see <%s>privacy policy</a> for details."),
@@ -149,7 +153,7 @@ if ($type == "signup") {
 }
 ?>
         <tr>
-            <td colspan="4"><h2><?php echo _("Profile settings") ?></h2>
+            <td colspan="4"><h2><?php echo _("Profile settings"); ?></h2>
 <?php
 if ($type == "signup") {
     echo _("You can easily change these later by clicking on <i>Settings</i>.");
@@ -183,15 +187,15 @@ if ($type == "signup") {
                 ); ?>
                 </span>
                 <span id="privacyY" style="display: inline"><?php
-                    echo _("<b>Public</b> profiles let others see your flight map and general statistics, but flight details like exact dates and class of service are not revealed.") ?>
+                    echo _("<b>Public</b> profiles let others see your flight map and general statistics, but flight details like exact dates and class of service are not revealed."); ?>
                 </span>
                 <span id="privacyO" style="display: none"><?php
-                    echo _("<b>Open</b> profiles let others see, but not edit, your detailed flight data as well.") ?>
+                    echo _("<b>Open</b> profiles let others see, but not edit, your detailed flight data as well."); ?>
                 </span>
             </td>
           </tr>
           <tr>
-            <td class="key"><?php echo _("Flight editor") ?></td>
+            <td class="key"><?php echo _("Flight editor"); ?></td>
             <td class="value">
                 <input type="radio" name="editor" value="B" onClick="JavaScript:changeEditor('B')" <?php
                     condOut($settings, 'editor', 'B', 'CHECKED');
@@ -203,37 +207,39 @@ if ($type == "signup") {
             <td class="desc">
                 <span id="basiceditor" style="display: <?php
                     condOut($settings, 'editor', 'B', 'inline', 'none'); ?>"><?php
-                    echo _("The <B>Basic</b> editor is quick and easy: from where, to where, the date and optionally the airline, up to four flights at a time. The fastest way to build up your map!") ?>
+                    echo _("The <B>Basic</b> editor is quick and easy: from where, to where, the date and optionally the airline, up to four flights at a time. The fastest way to build up your map!"); ?>
                 </span>
                 <span id="detaileditor" style="display: <?php
                     condOut($settings, 'editor', 'D', 'inline', 'none'); ?>"><?php
-                    echo _("The <B>Detailed</b> editor lets you add class of service, seat numbers, plane models and registrations, freeform notes and much more. Perfect for aviation fans and planespotters.") ?>
+                    echo _("The <B>Detailed</b> editor lets you add class of service, seat numbers, plane models and registrations, freeform notes and much more. Perfect for aviation fans and planespotters."); ?>
                 </span>
             </td>
           </tr>
           <tr>
-            <td class="key"><?php echo _("Distances") ?></td>
+            <td class="key"><?php echo _("Distances"); ?></td>
             <td class="value">
               <input type="radio" name="units" value="M" <?php
-                condOut($settings, 'units', 'M', 'CHECKED'); echo ">" . _("Miles") ?><br>
+                condOut($settings, 'units', 'M', 'CHECKED');
+                echo ">" . _("Miles"); ?><br>
               <input type="radio" name="units" value="K" <?php
-                condOut($settings, 'units', 'K', 'CHECKED'); echo ">" . _("Kilometers") ?><br>
+                condOut($settings, 'units', 'K', 'CHECKED');
+                echo ">" . _("Kilometers"); ?><br>
             </td>
-            <td class="desc"><?php echo _("Preferred unit for flight distances") ?></td>
+            <td class="desc"><?php echo _("Preferred unit for flight distances"); ?></td>
           </tr>
 
 <?php if ($type == "settings") { ?>
           <tr class="gold">
-            <td class="key"><?php echo _("Guest password") ?></td>
+            <td class="key"><?php echo _("Guest password"); ?></td>
             <td class="value"><input type="password" name="guestpw" size="20" DISABLED></td>
             <td class="desc" colspan=2>
                 <a href="/donate" target="_blank"><img src="/img/gold-star-mini.png" alt="Gold star" title="<?php
-                    echo _("Gold Elite feature") ?>" height=17 width=17></a> <?php
-                    echo _("Password protect your Private profile, so only friends and family can see it.") ?>
+                    echo _("Gold Elite feature"); ?>" height=17 width=17></a> <?php
+                    echo _("Password protect your Private profile, so only friends and family can see it."); ?>
             </td>
           </tr>
           <tr class="gold">
-            <td class="key"><?php echo _("Default view") ?></td>
+            <td class="key"><?php echo _("Default view"); ?></td>
             <td class="value">
                 <input type="radio" name="startpane" value="H" DISABLED <?php
                     condOut($settings, 'startpane', 'H', 'CHECKED');
@@ -252,13 +258,13 @@ if ($type == "signup") {
             </td>
           </tr>
           <tr>
-             <td colspan="4"><h2><?php echo _("Manage flights") ?></h2></td>
+             <td colspan="4"><h2><?php echo _("Manage flights"); ?></h2></td>
           </tr>
           <tr>
             <td></td>
             <td class="value">
-              <input type='button' value='<?php echo _("Backup to CSV") ?>' onClick='javascript:backupFlights()'>
-              <input type='button' value='<?php echo _("Delete all flights") ?>' onClick='javascript:validate("RESET")'>
+              <input type='button' value='<?php echo _("Backup to CSV"); ?>' onClick='javascript:backupFlights()'>
+              <input type='button' value='<?php echo _("Delete all flights"); ?>' onClick='javascript:validate("RESET")'>
             </td>
             <td class="desc" colspan=2><?php
                 printf(
@@ -268,35 +274,35 @@ if ($type == "signup") {
             </td>
           </tr>
           <tr>
-            <td colspan="4"><h2><?php echo _("Change password") ?></h2></td>
+            <td colspan="4"><h2><?php echo _("Change password"); ?></h2></td>
           </tr>
           <tr>
-            <td class="key"><?php echo _("Current password") ?></td>
+            <td class="key"><?php echo _("Current password"); ?></td>
             <td class="value"><input type="password" name="oldpw" size="20">
-            <input type="hidden" name="username" value="<?php echo $_SESSION['name']?>"></td>
-            <td class="desc" colspan=2><?php echo _("You only need to enter this if changing your password.") ?></td>
+            <input type="hidden" name="username" value="<?php echo $_SESSION['name']; ?>"></td>
+            <td class="desc" colspan=2><?php echo _("You only need to enter this if changing your password."); ?></td>
           </tr>
           <tr>
-            <td class="key"><?php echo _("New password") ?></td>
+            <td class="key"><?php echo _("New password"); ?></td>
             <td class="value"><input type="password" name="pw1" size="20"></td>
           </tr>
           <tr>
-            <td class="key"><?php echo _("New password again") ?>&nbsp;</td>
+            <td class="key"><?php echo _("New password again"); ?>&nbsp;</td>
             <td class="value"><input type="password" name="pw2" size="20"></td>
           </tr>
           <tr>
             <td class="key"></td>
             <td class="value">
-              <input type="button" value="<?php echo _("Save changes") ?>" onClick="validate('EDIT')">
-              <input type="button" value="<?php echo _("Cancel") ?>" onClick="location.href = '/'">
+              <input type="button" value="<?php echo _("Save changes"); ?>" onClick="validate('EDIT')">
+              <input type="button" value="<?php echo _("Cancel"); ?>" onClick="location.href = '/'">
             </td>
           </tr>
 <?php } else { ?>
           <tr>
             <td class="key"></td>
             <td class="value">
-              <input type="button" value="<?php echo _("Sign me up!") ?>" onClick="validate('NEW')">
-              <small><A href="/"><?php echo _("Cancel") ?></a></small>
+              <input type="button" value="<?php echo _("Sign me up!"); ?>" onClick="validate('NEW')">
+              <small><A href="/"><?php echo _("Cancel"); ?></a></small>
             </td>
           </tr>
 <?php } ?>
