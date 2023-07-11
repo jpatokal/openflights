jpatokal/openflights

commit 09b36136e15bf31f1f4b10c98f2ab99b3e6980d6

Use KM_PER_MILE const
Deprecate $KMPERMILE

php/top10.php
@@ -18,7 +18,7 @@ $mode = $_POST["mode"];
 switch($mode) {
  case "D":
    $mode = "SUM(distance)";
-   if($units == "K") $mode = "ROUND($mode * $KMPERMILE)";
+   if($units == "K") $mode = "ROUND($mode * KM_PER_MILE)";
    break;
 
  case "F":

badge/banner.php
@@ -73,7 +73,7 @@ $result = $sth->execute([$uid]);
 if ($result && $row = $sth->fetch()) {
     $distance = $row["distance"];
     if ($units == "K") {
-        $distance *= $KMPERMILE;
+        $distance *= KM_PER_MILE;
         $units = "km";
     } else {
         $units = "miles";

php/flights.php
@@ -232,7 +232,7 @@ while ($row = $sth->fetch()) {
 
         // Convert mi to km if units=K *and* we're not loading a single flight
         if($units == "K" && (!$fid || $fid == "0")) {
-            $row["distance"] = round($row["distance"] * $KMPERMILE);
+            $row["distance"] = round($row["distance"] * KM_PER_MILE);
         }
 
         printf(

php/helper.php
@@ -1,4 +1,8 @@
 <?php
+
+/**
+ * @deprecated use KM_PER_MILE const
+ */
 $KMPERMILE = "1.609344"; // must be a string or locale may turn this into a comma!
 const KM_PER_MILE = "1.609344";
 

php/map.php
@@ -129,7 +129,7 @@ if($row = $sth->fetch()) {
   $distance = $row["distance"];
   if(!$distance) $distance = "0";
   if($_SESSION["units"] == "K") {
-    $distance = round($distance * $KMPERMILE) . " " . _("km");
+    $distance = round($distance * KM_PER_MILE) . " " . _("km");
   } else {
     $distance = $distance . " " . _("miles");
   }

php/stats.php
@@ -60,7 +60,7 @@ $array = array();
 $units = $_SESSION["units"];
 if($units == "K") {
   $unit = _("km");
-  $multiplier = "* " . $KMPERMILE;
+  $multiplier = "* " . KM_PER_MILE;
 } else {
   $unit = _("mi");
   $multiplier = "";
@@ -80,7 +80,7 @@ if($row = $sth->fetch()) {
   $array += $row;
 }
 $array["avg_distance"] .= " " . $unit;
-$array["localedist"] = round($array["distance"] * ($units == "K" ? $KMPERMILE : 1)) . " " . $unit;
+$array["localedist"] = round($array["distance"] * ($units == "K" ? KM_PER_MILE : 1)) . " " . $unit;
 print json_encode($array) . "\n";
 
 // longest and shortest

commit 4bd086d48d8e098772fdbc16b47c3560fdaabd1c

top10.php: Fix distance in KM
Caused by 09b36136e15bf31f1f4b10c98f2ab99b3e6980d6

php/top10.php
@@ -35,7 +35,7 @@ switch ($mode) {
         $mode = "SUM(distance)";
         $data["countUnit"] = "miles";
         if ($units == "K") {
-            $mode = "ROUND($mode * KM_PER_MILE)";
+            $mode = "ROUND($mode * " . KM_PER_MILE . ")";
             $data["countUnit"] = "kilometers";
         }
         break;