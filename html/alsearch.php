8dc3a43f4874b4059d6bf8ab876ef3af76c0f0d6
Sam Reed <sam@reedyboy.net>
2023-07-11 19:21:39 +0000

    Move unrelated code from greatcircle.js
    Fixes https://github.com/jpatokal/openflights/issues/1270

--- html/alsearch.php
+++ html/alsearch.php
@@ -16,7 +16,7 @@ $logged_in = $uid && !empty($uid);
     <link rel="gettext" type="application/x-po" href="/locale/<?php echo $locale;?>/LC_MESSAGES/messages.po" />
     <link rel="icon" type="image/png" href="/img/icon_favicon.png"/>
 
-    <script type="text/javascript" src="/js/greatcircle.js"></script>
+    <script type="text/javascript" src="/openflights.js"></script>
     <script type="text/javascript" src="/js/Gettext.min.js"></script>
     <script type="text/javascript" src="/js/alsearch.js"></script>
   </head>
