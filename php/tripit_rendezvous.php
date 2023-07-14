68cd7f56d5cbe98f8195f3f98160a5a5396c9974
Sam Reed <sam@reedyboy.net>
2023-07-14 20:53:20 +0100

    tripit_rendezvous.php: Use bundled jquery
    Follows up e7dba5f69641fcac7d2462fcd87a7136f96092bd
    
    Fixes #1256
    
    But other JS loaded from google still :(

--- php/tripit_rendezvous.php
+++ php/tripit_rendezvous.php
@@ -18,7 +18,7 @@ if (!$uid || empty($uid)) {
     <link rel="stylesheet" href="/openflights.css" type="text/css">
     <link rel="stylesheet" href="/css/tripit.css" type="text/css">
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/themes/ui-lightness/jquery-ui.css" type="text/css">
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script type="text/javascript" src="<?php echo fileUrlWithDate("/js/jquery.min.js") ?>></script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
     <script type="text/javascript" src="/js/tripit.js"></script>
   </head>
