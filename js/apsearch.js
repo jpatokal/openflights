jpatokal/openflights

commit c03f72006444dfc2485eec28ba936d3d74d1631d

composer.json: Upgrade knplabs/github-api
https://github.com/jpatokal/openflights/issues/1158

php/apsearch.php
@@ -38,7 +38,7 @@ if($action == "RECORD") {
     if($apid && $apid != "") {
       $filters[] = "apid=?";
       $filterParams[] = $apid;
-    } 
+    }
     if($iata != "") {
       $filters[] = " iata=?";
       $filterParams[] = $iata;
@@ -58,7 +58,7 @@ if($action == "RECORD") {
     }
   }
 
-  if(! $apid || $apid == "") {    
+  if(! $apid || $apid == "") {
     $sql = "INSERT INTO airports(name,city,country,iata,icao,x,y,elevation,timezone,dst,uid) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $params = [
       $airport, $city, $country,
@@ -120,7 +120,7 @@ TXT;
     }
     $identifier = ($icao == "") ? $iata : $icao;
     $github = new \Github\Client();
-    $github->authenticate($GITHUB_ACCESS_TOKEN, NULL, Github\Client::AUTH_HTTP_TOKEN);
+    $github->authenticate($GITHUB_ACCESS_TOKEN, NULL, Github\AuthMethod::ACCESS_TOKEN);
 
     $issues = $github->api('search')->issues("repo:$GITHUB_USER/$GITHUB_REPO in:title $identifier");
     if(count($issues['items']) > 0) {
@@ -187,7 +187,7 @@ if($action == "LOAD") {
       }
     }
   }
-  
+
   // Disable this filter for DAFIF (no IATA data)
   if($iatafilter == "false" || $dbname == "airports_dafif") {
     $sql .= " 1=1"; // dummy
@@ -218,7 +218,7 @@ array_pop($rows);
 foreach($rows as &$row) {
   if($dbname == "airports_dafif" || $dbname == "airports_oa") {
     $row["country"] = $row["code"];
-  } 
+  }
   if($row["uid"] || $uid == $OF_ADMIN_UID ) {
     if($row["uid"] == $uid || $uid == $OF_ADMIN_UID) {
       $row["ap_uid"] = "own"; // editable

commit 4ddd534362bc2b6edc7df23b5e5f73da501c145b

Fixup GitHub issue submission
Leave comment about token expiring...

Display errors from apsearch in red

Helps towards https://github.com/jpatokal/openflights/issues/1314

Probably broken in https://github.com/jpatokal/openflights/commit/c03f72006444dfc2485eec28ba936d3d74d1631d

js/apsearch.js
@@ -61,11 +61,9 @@ function xmlhttpPost(strURL, offset, action) {
       if (strURL == URL_APSEARCH) {
         if (action == "SEARCH") {
           searchResult(self.xmlHttpReq.responseText);
-        }
-        if (action == "RECORD") {
+        } else if (action == "RECORD") {
           recordResult(self.xmlHttpReq.responseText);
-        }
-        if (action == "LOAD") {
+        } else if (action == "LOAD") {
           loadAirport(self.xmlHttpReq.responseText);
         }
       }
@@ -155,13 +153,9 @@ function xmlhttpPost(strURL, offset, action) {
             break;
         }
       }
-    }
-
-    if (action == "LOAD") {
+    } else if (action == "LOAD") {
       apid = offset; // ugly hack!
-    }
-
-    if (action == "RECORD") {
+    } else if (action == "RECORD") {
       if (airport == "") {
         alert(gt.gettext("Please enter an airport name."));
         form.airport.focus();
@@ -307,8 +301,9 @@ function xmlhttpPost(strURL, offset, action) {
             )
           )
         ) {
-          getElement("miniresultbox").innerHTML =
-            "<I>" + gt.gettext("Cancelled.") + "</I>";
+          var box = getElement("miniresultbox");
+          box.style.color = "FF0000";
+          box.innerHTML = "<p><i>" + gt.gettext("Cancelled.") + "</i></p>";
           return;
         }
       }
@@ -363,8 +358,9 @@ function xmlhttpPost(strURL, offset, action) {
         action;
     }
   }
-  getElement("miniresultbox").innerHTML =
-    "<I>" + gt.gettext(describe(action)) + "</I>";
+  var box = getElement("miniresultbox");
+  box.style.color = "#222";
+  box.innerHTML = "<p><i>" + gt.gettext(describe(action)) + "</i></p>";
   self.xmlHttpReq.send(query + "&offset=" + offset);
 }
 
@@ -431,19 +427,19 @@ function searchResult(str) {
       table += '<td style="text-align: right"><nobr>';
       if (offset - 10 >= 0) {
         table +=
-          '<INPUT id="b_back" type="button" value="<" onClick="doSearch(' +
+          '<input id="b_back" type="button" value="<" onClick="doSearch(' +
           (offset - 10) +
           ')">';
       } else {
-        table += '<INPUT type="button" value="<" disabled>';
+        table += '<input type="button" value="<" disabled>';
       }
       if (offset + 10 < max) {
         table +=
-          '<INPUT id="b_fwd" type="button" value=">" onClick="doSearch(' +
+          '<input id="b_fwd" type="button" value=">" onClick="doSearch(' +
           (offset + 10) +
           ')">';
       } else {
-        table += '<INPUT type="button" value=">" disabled>';
+        table += '<input type="button" value=">" disabled>';
       }
       table += "</nobr></td>";
     }
@@ -531,16 +527,19 @@ function searchResult(str) {
   }
   table += "</table>";
   table += disclaimer;
-  getElement("miniresultbox").innerHTML = table;
+  var box = getElement("miniresultbox");
+  box.style.color = "#222";
+  box.innerHTML = table;
 }
 
-// Load data from search result into form
+// Load data from the search result into the form
 function loadAirport(data) {
   var json = JSON.parse(data);
   if (json["status"] != 1 || json["max"] == 0) {
-    getElement("miniresultbox").innerHTML = gt.gettext(
-      "No matches found in this database."
-    );
+    var box = getElement("miniresultbox");
+    box.style.color = "#FF0000";
+    box.innerHTML =
+      "<p>" + gt.gettext("No matches found in this database.") + "</p>";
     return;
   }
   var col = json["airports"][0];
@@ -578,12 +577,16 @@ function loadAirport(data) {
   getElement("b_add").style.display = "none";
   getElement("b_edit").style.display = "inline";
   getElement("b_edit").disabled = true;
-  getElement("miniresultbox").innerHTML = "";
+  var box = getElement("miniresultbox");
+  box.innerHTML = "";
+  box.style.color = "#222";
 }
 
 // Did we manage to record the airport?
 function recordResult(str) {
   var json = JSON.parse(str);
+  // Success
+  var box = getElement("miniresultbox");
   if (json["status"] == "1") {
     alert(json["message"]);
     // Select newly minted airport and return to main
@@ -605,8 +608,11 @@ function recordResult(str) {
       "), " +
       country;
     selectAirport(data, name);
+    box.style.color = "#222";
+  } else {
+    box.style.color = "#FF0000";
   }
-  getElement("miniresultbox").innerHTML = json["message"];
+  box.innerHTML = "<p>" + json["message"] + "</p>";
   getElement("b_edit").disabled = true;
 }
 
@@ -638,7 +644,9 @@ function clearSearch() {
   getElement("b_add").style.display = "inline";
   getElement("b_add").disabled = true;
   getElement("b_edit").style.display = "none";
-  getElement("miniresultbox").innerHTML = "";
+  var box = getElement("miniresultbox");
+  box.style.color = "#222";
+  box.innerHTML = "";
 }
 
 function isLoggedIn() {

php/apsearch.php
@@ -1,5 +1,9 @@
 <?php
 
+use Github\Api\Issue;
+use Github\Api\Search;
+use Github\Client;
+
 require_once '../vendor/autoload.php';
 require_once '../php/locale.php';
 require_once '../php/db_pdo.php';
@@ -112,6 +116,15 @@ SQL;
             json_error("Editing airport failed.");
         }
     } else {
+        if (
+            // Check for empty strings, or default values as per config.php.sample
+            in_array($GITHUB_USER, [ "", "YOUR_USERNAME"] ) ||
+            in_array($GITHUB_ACCESS_TOKEN, ["", "YOUR_TOKEN"]) ||
+            $GITHUB_REPO == ''
+        ) {
+            json_error("Cannot submit edit request to GitHub; please check config!");
+        }
+
         $name = $_SESSION['name'];
         $newEdit = print_r(
             [
@@ -154,36 +167,40 @@ TXT;
             exit;
         }
         $identifier = ($icao == "") ? $iata : $icao;
-        $github = new \Github\Client();
-        $github->authenticate($GITHUB_ACCESS_TOKEN, null, Github\AuthMethod::ACCESS_TOKEN);
+        $github = new Client();
+        try {
+            $github->authenticate($GITHUB_ACCESS_TOKEN, null, Github\AuthMethod::ACCESS_TOKEN);
 
-        $issues = $github->api('search')->issues("repo:$GITHUB_USER/$GITHUB_REPO in:title $identifier");
-        if (count($issues['items']) > 0) {
-            // Existing issue, add comment
-            $issueNumber = $issues['items'][0]['number'];
-            //https://docs.github.com/en/rest/issues/comments?apiVersion=2022-11-28#create-an-issue-comment
-            $result = $github->api('issue')->comments()->create(
-                $GITHUB_USER,
-                $GITHUB_REPO,
-                $issueNumber,
-                ['body' => $body]
-            );
-        } else {
-            // New issue
-            // https://docs.github.com/en/rest/issues/issues?apiVersion=2022-11-28#create-an-issue
-            $result = $github->api('issue')->create(
-                $GITHUB_USER,
-                $GITHUB_REPO,
-                ['title' => $subject, 'body' => $body, 'labels' => ['airport']]
-            );
-            $issueNumber = $result['number'];
-        }
-        // TODO: Actually do error handling
-        if (true) {
+            $issues = $github->api('search')->issues("repo:$GITHUB_USER/$GITHUB_REPO is:issue in:title $identifier");
+
+            if (count($issues['items']) > 0) {
+                // Existing issue, add comment
+                $issueNumber = $issues['items'][0]['number'];
+                // https://docs.github.com/en/rest/issues/comments?apiVersion=2022-11-28#create-an-issue-comment
+                $result = $github->api('issue')->comments()->create(
+                    $GITHUB_USER,
+                    $GITHUB_REPO,
+                    $issueNumber,
+                    ['body' => $body]
+                );
+            } else {
+                // New issue
+                // https://docs.github.com/en/rest/issues/issues?apiVersion=2022-11-28#create-an-issue
+                $result = $github->api('issue')->create(
+                    $GITHUB_USER,
+                    $GITHUB_REPO,
+                    ['title' => $subject, 'body' => $body, 'labels' => ['airport']]
+                );
+                $issueNumber = $result['number'];
+            }
+
             $message = "Edit submitted for review on Github: Issue {$issueNumber}, {$result['html_url']}";
             json_success(["apid" => $apid, "message" => $message]);
-        } else {
-            json_error("Could not submit edit for review, please contact <a href='/about'>support</a>.");
+        } catch (GitHub\Exception\RuntimeException $ex) {
+            // $ex->code === 401 is Unauthorised
+            // Probably not localised...
+            json_error($ex->getMessage());
+            // json_error("Could not submit edit for review, please contact <a href='/about'>support</a>.");
         }
     }
     exit;

php/config.php.sample
@@ -4,10 +4,17 @@ $dbname = "flightdb2";
 $user = "openflights";
 $password = "";
 
-// Authentication for creating Github issues from suggested data updates
-// https://help.github.com/articles/creating-an-access-token-for-command-line-use/
+// Authentication for creating Github issues from suggested data updates.
+// https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#fine-grained-personal-access-tokens
+// Note, these expire at max a year...
+
+// Repo used will end up being $GITHUB_USER/$GITHUB_REPO.
 $GITHUB_REPO = "openflights";
+// This is the username/organisation that the repo lives under.
+// Not actually used for login.
 $GITHUB_USER = "YOUR_USERNAME";
+// The user that this belongs to (which does not have to be the same as $GITHUB_USER), that issues
+// will be created as, as comments left as.
 $GITHUB_ACCESS_TOKEN = "YOUR_TOKEN";
 
 // Enable non-English locales? (true/false)