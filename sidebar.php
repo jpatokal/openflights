64833b78e898f8f402f1154d34681e01d2e4353e
Sam Reed <sam@reedyboy.net>
2023-07-19 20:37:54 +0100

    Add some more language selectors
    Fix some more stray syntax and untranslated messages

--- sidebar.php
+++ sidebar.php
@@ -3,10 +3,11 @@ require_once "./php/locale.php";
 ?>
 
 <center>
-  <a href="/"><img src="/img/openflights-logo.png" alt="<?php echo _('OpenFlights logo'); ?>" width="220" height="31" style="margin: 10px 10px 0px 10px"/></a><br>
-  <h6> <a href="/about">About</a> |
-    <span id="helplink" style="display: none"><a href="#help" onclick="JavaScript:openPane('help');"><?php echo _('Help'); ?></a> |</span>
-    <a href="/faq">FAQ</a> |
-    <a href="https://blog.openflights.org">Blog</a>
-  </h6>
+    <a href="/"><img src="/img/openflights-logo.png" alt="<?php echo _('OpenFlights logo'); ?>" width="220" height="31" style="margin: 10px 10px 0px 10px"/></a><br>
+    <h6>
+        <a href="/about"><?php echo _('About'); ?></a> |
+        <span id="helplink" style="display: none"><a href="#help" onclick="JavaScript:openPane('help');"><?php echo _('Help'); ?></a> |</span>
+        <a href="/faq"><?php echo _('FAQ'); ?></a> |
+        <a href="https://blog.openflights.org"><?php echo _('Blog'); ?></a>
+    </h6>
 </center>
