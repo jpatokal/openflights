jpatokal/openflights

commit 4c91eeda44d3747955abfd21170c2f12784d004a

Migrate airport search fully to JSON, add client-side testing, lots of minor UI tweaks

js/apsearch.js
@@ -31,6 +31,10 @@ function doRecord(offset) {
   xmlhttpPost(URL_APSEARCH, offset, "RECORD");
 }
 
+function doLoad(apid) {
+  xmlhttpPost(URL_APSEARCH, apid, "LOAD");
+}
+
 function xmlhttpPost(strURL, offset, action) {
   var xmlHttpReq = false;
   var self = this;
@@ -55,8 +59,7 @@ function xmlhttpPost(strURL, offset, action) {
 	  recordResult(self.xmlHttpReq.responseText);
 	}
 	if(action == "LOAD") {
-	  loadAirport(self.xmlHttpReq.responseText.split("\n")[1]);
-	  document.getElementById("miniresultbox").innerHTML = "";
+	  loadAirport(self.xmlHttpReq.responseText);
 	}
       }
     }
@@ -214,7 +217,7 @@ function xmlhttpPost(strURL, offset, action) {
 	  " (IATA: " + (iata == "" ? "N/A" : iata)  + ", ICAO: " + (icao == "" ? "N/A" : icao) + ")";
 	quad = (parseFloat(y) < 0 ? "SOUTH" : "NORTH") + "-" + (parseFloat(x) < 0 ? "WEST" : "EAST");
 	if(! confirm(Gettext.strargs(gt.gettext("Are you sure you want to add %1 as a new airport, located in the %2 quadrant of the world?  Please double-check the name, airport codes and exact coordinates before confirming."), [desc, quad]))) {
-	  document.getElementById("miniresultbox").innerHTML = "<I>" + gt.gettext("Cancelled.") + "</I>";
+	  getElement("miniresultbox").innerHTML = "<I>" + gt.gettext("Cancelled.") + "</I>";
 	  return;
 	}
       }
@@ -240,7 +243,7 @@ function xmlhttpPost(strURL, offset, action) {
 	'action=' + action;
     }
   }
-  document.getElementById("miniresultbox").innerHTML = "<I>" + gt.gettext(describe(action)) + "</I>";
+  getElement("miniresultbox").innerHTML = "<I>" + gt.gettext(describe(action)) + "</I>";
   self.xmlHttpReq.send(query + '&offset=' + offset);
 }
 
@@ -259,7 +262,8 @@ function describe(action) {
  * Display results of search
  */
 function searchResult(str) {
-  var airports = str.split("\n");
+  var json = jsonParse(str);
+  var airports = json["airports"];
   var table = "<table width=95% cellspacing=0>";
   var offset, sql;
   var db = document.forms['searchform'].db.value;
@@ -269,87 +273,87 @@ function searchResult(str) {
     table += "<tr><td colspan=2><i><font color='red'>" + warning + "</font></i></td></tr>";
     warning = null;
   }
-  for(a in airports) {
-    // First line contains header info
-    if(a == 0) {
-      var col = airports[a].split(";");
-      offset = parseInt(col[0]);
-      max = col[1];
-      if(max == 0) {
-	table += "<tr><td><i>" + gt.gettext("No matches found in this database.") + "<br><ul>";
-	if(document.forms['searchform'].iatafilter.checked) {
-	  table += "<li>" + gt.gettext("Try unchecking 'Show only major airports' and search again.");
-	}
-	if(document.forms['searchform'].db.value != DB_OURAIRPORTS) {
-	  table += "<li>" + gt.gettext("Switch to the OurAirports database and search again.");
-	}
-	table += "</ul></td></tr>";
-	break;
-      }
-      table += "<tr><td><b>" + Gettext.strargs(gt.gettext("Results %1 to %2 of %3"), [offset+1, Math.min(offset+10, max), max]) + "</b><br></td>";
 
-      if(max > 10) {
-	table += "<td style=\"text-align: right\"><nobr>";
-	if(offset - 10 >= 0) {
-	  table += "<INPUT id=\"b_back\" type=\"button\" value=\"<\" onClick=\"doSearch(" + (offset-10) + ")\">";
-	} else {
-	  table += "<INPUT type=\"button\" value=\"<\" disabled>";
-	}
-	if(offset + 10 < max) {
-	  table += "<INPUT id=\"b_fwd\" type=\"button\" value=\">\" onClick=\"doSearch(" + (offset+10) + ")\">";
-	} else {
-	  table += "<INPUT type=\"button\" value=\">\" disabled>";
-	}
-	table += "</nobr></td>";
-      }
-      table += "</tr>";
-      continue;
+  offset = json["offset"];
+  max = json["max"];
+  if(max == 0) {
+    table += "<tr><td><i>" + gt.gettext("No matches found in this database.") + "<br><ul>";
+    if(document.forms['searchform'].iatafilter.checked) {
+      table += "<li>" + gt.gettext("Try unchecking 'Show only major airports' and search again.");
     }
-
-
-    // Meat of the table
-    var col = jsonParse(airports[a]);
-    if(a % 2 == 1) {
-      bgcolor = "#fff";
-    } else {
-      bgcolor = "#ddd";
+    if(document.forms['searchform'].db.value != DB_OURAIRPORTS) {
+      table += "<li>" + gt.gettext("Switch to the OurAirports database and search again.");
     }
-    switch(col["ap_uid"]) {
-    case "user":
-      bgcolor = "#fdd";
-      disclaimer = "<br><span style='background-color: " + bgcolor + "'>" + gt.gettext("Airports in pink have been added by users of OpenFlights.") + "</span>";
-      break;
-
-    case "own":
-      bgcolor = "#ddf";
-      disclaimer = "<br><span style='background-color: " + bgcolor + "'>" + gt.gettext("Airports in blue have been added by you and can be edited.") + "<span>";
-      break;
-    }
-    table += "<tr><td style='background-color: " + bgcolor + "'>" + col["ap_name"] + "</td>";
-    if(db == DB_OPENFLIGHTS && isEditMode()) {
-      // code:apid:x:y:tz:dst
-      id = (col["iata"] != "" ? col["iata"] : col["icao"]) + ":" + col["apid"] + ":" + col["x"] + ":" + col["y"] +
-	":" + col["timezone"] + ":" + col["dst"];
-      table += "<td style='text-align: right; background-color: " + bgcolor + "'><INPUT type='button' value='" + gt.gettext("Select") + "' onClick='selectAirport(\"" + id + "\",\"" + encodeURIComponent(col["ap_name"]) + "\")'></td>";
-    }
-    if(db != DB_OPENFLIGHTS || col["ap_uid"] == "own" || ! isEditMode()) {
-      if(col["ap_uid"] == "own" && db == DB_OPENFLIGHTS) {
-	label = gt.gettext("Edit");
+    table += "</ul></td></tr>";
+  } else {
+    table += "<tr><td><b>" + Gettext.strargs(gt.gettext("Results %1 to %2 of %3"), [offset+1, Math.min(offset+10, max), max]) + "</b><br></td>";
+    if(max > 10) {
+      table += "<td style=\"text-align: right\"><nobr>";
+      if(offset - 10 >= 0) {
+        table += "<INPUT id=\"b_back\" type=\"button\" value=\"<\" onClick=\"doSearch(" + (offset-10) + ")\">";
       } else {
-	label = gt.gettext("Load");
+        table += "<INPUT type=\"button\" value=\"<\" disabled>";
       }
-      table += "<td style='text-align: right; background-color: " + bgcolor + "'><INPUT type='button' value='" + label + "' onClick='loadAirport(\"" + encodeURIComponent(airports[a]) + "\")'></td>";
+      if(offset + 10 < max) {
+        table += "<INPUT id=\"b_fwd\" type=\"button\" value=\">\" onClick=\"doSearch(" + (offset+10) + ")\">";
+      } else {
+        table += "<INPUT type=\"button\" value=\">\" disabled>";
+      }
+      table += "</nobr></td>";
     }
     table += "</tr>";
+
+    for(a in airports) {
+      // Meat of the table
+      
+      var col = airports[a];
+      if(a % 2 == 1) {
+        bgcolor = "#fff";
+      } else {
+        bgcolor = "#ddd";
+      }
+      switch(col["ap_uid"]) {
+      case "user":
+        bgcolor = "#fdd";
+        disclaimer = "<br><span style='background-color: " + bgcolor + "'>" + gt.gettext("Airports in pink have been added by users of OpenFlights.") + "</span>";
+        break;
+
+      case "own":
+        bgcolor = "#ddf";
+        disclaimer = "<br><span style='background-color: " + bgcolor + "'>" + gt.gettext("Airports in blue have been added by you and can be edited.") + "<span>";
+        break;
+      }
+      table += "<tr><td style='background-color: " + bgcolor + "'>" + col["ap_name"] + "</td>";
+      if(db == DB_OPENFLIGHTS && isEditMode()) {
+        // code:apid:x:y:tz:dst
+        id = (col["iata"] != "" ? col["iata"] : col["icao"]) + ":" + col["apid"] + ":" + col["x"] + ":" + col["y"] +
+	  ":" + col["timezone"] + ":" + col["dst"];
+        table += "<td style='text-align: right; background-color: " + bgcolor + "'><INPUT type='button' value='" + gt.gettext("Select") + "' onClick='selectAirport(\"" + id + "\",\"" + encodeURIComponent(col["ap_name"]) + "\")'></td>";
+      }
+      if(db != DB_OPENFLIGHTS || col["ap_uid"] == "own" || ! isEditMode()) {
+        if(col["ap_uid"] == "own" && db == DB_OPENFLIGHTS) {
+	  label = gt.gettext("Edit");
+        } else {
+	  label = gt.gettext("Load");
+        }
+        table += "<td style='text-align: right; background-color: " + bgcolor + "'><INPUT type='button' value='" + label + "' onClick='doLoad(\"" + col["apid"] + "\")'></td>";
+      }
+      table += "</tr>";
+    }
   }
   table += "</table>";
   table += disclaimer;
-  document.getElementById("miniresultbox").innerHTML = table;
+  getElement("miniresultbox").innerHTML = table;
 }
 
 // Load data from search result into form
 function loadAirport(data) {
-  var col = jsonParse(unescape(data));
+  var json = jsonParse(data);
+  if(json["status"] != 1 || json["max"] == 0) {
+    getElement("miniresultbox").innerHTML = gt.gettext("No matches found in this database.");
+    return;
+  }
+  var col = json["airports"][0];
 
   var form = document.forms['searchform'];
   form.airport.value = col["name"];
@@ -377,26 +381,18 @@ function loadAirport(data) {
     }
   }
 
-  if(col["apid"]) {
-    form.apid.value = col["apid"];
-    document.getElementById('b_add').style.display = "none";
-    document.getElementById('b_edit').style.display = "inline";
-  } else {
-    form.apid.value = "";
-    document.getElementById('b_add').style.display = "inline";
-    document.getElementById('b_edit').style.display = "none";
-  }
+  form.apid.value = col["apid"];
+  getElement('b_add').style.display = "none";
+  getElement('b_edit').style.display = "inline";
+  getElement("b_edit").disabled = true;
+  getElement("miniresultbox").innerHTML = "";
 }
 
 // Did we manage to record the airport?
 function recordResult(str) {
-  var col = str.split(";");
-  // Error?
-  if(col[0] != "1") {
-    document.getElementById("miniresultbox").innerHTML = col[1];
+  var json = jsonParse(str);
+  if(json["status"] != "1") {
   } else {
-    document.getElementById("miniresultbox").innerHTML = col[2];
-
     // Select newly minted airport and return to main
     var form = document.forms['searchform'];
     var iata = form.iata.value;
@@ -405,12 +401,24 @@ function recordResult(str) {
     // code:apid:x:y
     code = (iata != "" ? iata : form.icao.value);
     // city-airport (code), country
-    data = code + ":" + col[1] + ":" + form.x.value + ":" + form.y.value;
+    data = code + ":" + json["apid"] + ":" + form.x.value + ":" + form.y.value;
     name = form.city.value + "-" + form.airport.value + " (" + code + "), " + country;
     selectAirport(data, name);
   }
+  getElement("miniresultbox").innerHTML = json["message"];
+  getElement("b_edit").disabled = true;
 }
 
+function setEdited() {
+  if(isLoggedIn()) {
+    if(getElement("b_edit").style.display == "inline") {
+      getElement("b_edit").disabled = false;
+    } else {
+      getElement("b_add").disabled = false;
+    }
+  }
+}
+    
 // Clear form -- everything *except* database
 function clearSearch() {
   var form = document.forms['searchform'];
@@ -426,25 +434,33 @@ function clearSearch() {
   form.dst.selectedIndex = 0;
   form.apid.value = "";
   form.iatafilter.checked = true;
-  document.getElementById('b_add').style.display = "inline";
-  document.getElementById('b_edit').style.display = "none";
-  document.getElementById("miniresultbox").innerHTML = "";
+  getElement('b_add').style.display = "inline";
+  getElement('b_add').disabled = true;
+  getElement('b_edit').style.display = "none";
+  getElement("miniresultbox").innerHTML = "";
 }
 
-function isEditMode() {
+function isLoggedIn() {
   if(! parent.opener || ! parent.opener.addNewAirport) {
     // If airport search was loaded without OpenFlights, we're not in edit mode
     return false;
   } else {
-    // Else we check with mommy
-    return parent.opener.isEditMode();
+    return parent.opener.logged_in;
   }
+}
+
+function isEditMode() {
+  return isLoggedIn() && parent.opener.isEditMode();
 }
 
 // Airport selected, kick it back to main window and close this
 function selectAirport(data, name) {
   parent.opener.addNewAirport(data, unescape(name));
   window.close();
+}
+
+function getElement(id) {
+  return document.getElementById(id);
 }
 
 // A dupe from openflights.js...

openflights.js
@@ -1893,12 +1893,14 @@ function popNewAirport(type, apid) {
 // (new for this map, that is, not necessarily DB)
 function addNewAirport(data, name) {
   var element = input_toggle;
-  $(element).value = name;
-  $(element).style.color = '#000000';
-  $(element + 'id').value = data;
-  replicateSelection(element);
-  markAirport(element);
-  markAsChanged(true);
+  if(input_toggle) {
+    $(element).value = name;
+    $(element).style.color = '#000000';
+    $(element + 'id').value = data;
+    replicateSelection(element);
+    markAirport(element);
+    markAsChanged(true);
+  }
 }
 
 // Handle the "add new airlines" buttons

php/apsearch.php
@@ -1,10 +1,9 @@
 <?php
 require_once("../php/locale.php");
 require_once("../php/db.php");
+require_once("../php/helper.php");
 
 header("Content-type: text/html");
-
-include 'helper.php';
 
 $airport = $_POST["name"];
 $iata = $_POST["iata"];
@@ -27,8 +26,7 @@ $uid = $_SESSION["uid"];
 
 if($action == "RECORD") {
   if(!$uid or empty($uid)) {
-    printf("0;" . _("Your session has timed out, please log in again."));
-    exit;
+    json_error("Your session has timed out, please log in again.");
   }
 
   // Check for potential duplicates (unless admin)
@@ -83,14 +81,14 @@ if($action == "RECORD") {
 		   mysql_real_escape_string($apid));
   }
   if(empty($duplicates)) {
-    mysql_query($sql, $db) or die('0;' . _("Adding new airport failed:") . ' ' . $sql);
+    mysql_query($sql, $db) or json_error("Adding new airport failed:", $sql);
     if(! $apid || $apid == "") {
-      printf('1;' . mysql_insert_id() . ";" . _("New airport successfully added."));
+      json_success(array("apid" => mysql_insert_id(), "message" => "New airport successfully added."));
     } else {
       if(mysql_affected_rows() == 1) {
-        printf('1;' . $apid . ';' . _("Airport successfully edited."));
+	json_success(array("apid" => $apid, "message" => "Airport successfully edited."));
       } else {
-        printf('0;' . _("Editing airport failed:") . ' ' . $sql);
+	json_error("Editing airport failed:", $sql);
       }
     }
   } else {
@@ -102,9 +100,9 @@ if($action == "RECORD") {
       exit;
     }
     if (mail($email, $subject, $body, $headers)) {
-      printf('1;' . $apid . ";" . _("Edit submitted for review.  If you have registered an e-mail address, you will be notified when the edit is reviewed."));
+      json_success(array("apid" => $apid, "message" => "Edit submitted for review.  If you have registered an e-mail address, you will be notified when the edit is reviewed."));
     } else {
-      printf('0;' . _("Could not submit edit for review, please contact <a href='/about'>support</a>."));
+      json_error("Could not submit edit for review, please contact <a href='/about'>support</a>.");
     }
   }
   exit;
@@ -157,20 +155,23 @@ if(! $offset) {
   $offset = 0;
 }
 
+// Check result count
 $sql2 = str_replace("*", "COUNT(*)", $sql);
-$result2 = mysql_query($sql2, $db) or die ('0;Operation ' . $param . ' failed: ' . $sql2);
+$result2 = mysql_query($sql2, $db) or json_error('Operation ' . $param . ' failed: ' . $sql2);
 if($row = mysql_fetch_array($result2, MYSQL_NUM)) {
   $max = $row[0];
 }
-printf("%s;%s", $offset, $max);
+$response = array("status" => 1, "offset" => $offset, "max" => $max);
 
+// Fetch airport data
 $sql .= " ORDER BY name LIMIT 10 OFFSET " . $offset;
-$result = mysql_query($sql, $db) or die ('0;Operation ' . $param . ' failed: ' . $sql);
-while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+$result = mysql_query($sql, $db) or die (json_encode(array("status" => 0, "message" => 'Operation ' . $param . ' failed: ' . $sql)));
+while ($rows[] = mysql_fetch_assoc($result, MYSQL_ASSOC));
+array_pop($rows);
+foreach($rows as &$row) {
   if($dbname == "airports_dafif" || $dbname == "airports_oa") {
     $row["country"] = $row["code"];
-  }
- 
+  } 
   if($row["uid"] || $uid == $OF_ADMIN_UID ) {
     if($row["uid"] == $uid || $uid == $OF_ADMIN_UID) {
       $row["ap_uid"] = "own"; // editable
@@ -182,7 +183,7 @@ while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
   }
   $row["ap_name"] = format_airport($row);
   unset($row["uid"]);
-  print "\n" . json_encode($row);
 }
-
+$response['airports'] = $rows;
+print json_encode($response);
 ?>

php/helper.php
@@ -5,6 +5,22 @@ $modes = array ("F" => "Flight", "T" => "Train", "S" => "Ship", "R" => "Road tri
 $modeOperators = array ("F" => "airline", "T" => "railway", "S" => "shipping company", "R" => "road transport company");
 
 //
+// End with JSON-formatted data, localized message and a successful status
+//
+function json_success($data) {
+  $data["status"] = 1;
+  $data["message"] = _($message);
+  die(json_encode($data));
+}
+
+//
+// Abort with a JSON-formatted localized error message
+//
+function json_error($msg, $detail='') {
+  die (json_encode(array("status" => 0, "message" => _($msg) . ' ' . $detail)));
+}
+
+//
 // Standard formatting of airport data
 // Input: row: associative array containing iata, icao
 // Output: " code : apid : x : y : timezone : dstrule "

test/client/AirportSearchTest.php
@@ -5,31 +5,76 @@ class AirportSearchTest extends OpenFlightsSeleniumTestCase
 {
   public function testAnonAirportSearch()
   {
+    global $airport;
+
     $this->open("/html/apsearch");
     $this->assertTextPresent("Airport search");
+    $this->assertTextPresent("Please log in to enable editing.");
     $this->assertNotEditable("//input[@value='Save changes']");
 
-    $this->type("iata", "LIF");
+    $this->type("iata", $airport["iata"]);
     $this->click("//input[@value='Search']");
 
-    $this->assertTextPresent("Lifou (LIF), New Caledonia");
+    $this->assertTextPresent($airport['name']);
     $this->click("//input[@value='Load']");
 
-    $this->assertValue("airport", "Lifou");
+    $this->assertValue("airport", $airport['name']);
   }
 
-  public function testLoggedInAirportSearch()
+  public function testAnonAirportSearchPreload()
   {
-    global $settings;
+    global $airport;
+
+    $apid = db_apid(db_connect());
+
+    $this->open("/html/apsearch?apid=" . $apid);
+    $this->assertTextPresent("Airport search");
+    $this->assertTextPresent("Please log in to enable editing.");
+    $this->assertNotEditable("//input[@value='Save changes']");
+    sleep(1);
+    $this->assertTextNotPresent("Loading...");
+    $this->assertValue("iata", $airport['iata']);
+    $this->assertValue("apid", $apid);
+
+    $this->open("/html/apsearch?apid=garbage");
+    sleep(1);
+    $this->assertTextNotPresent("Loading...");
+    $this->assertValue("iata", "");
+  }
+
+  public function testLoggedInAirportEdit()
+  {
+    global $settings, $airport;
 
     $this->open('/');
     $this->type('name', $settings['name']);
     $this->type('pw', $settings['password']);
     $this->click('loginbutton');
+    sleep(1);
 
-    $this->open("/html/apsearch");
-    $this->assertTextPresent("Airport search");
+    $this->click("//input[@value='New flight']");
+    $this->click("css=#row1 > td > img[title=Airport search]");
+
+    $this->selectWindow('OpenFlights: Airport search');
+    $this->assertNotEditable("//input[@value='Save changes']");
+
+    $this->type("iata", $airport["iata"]);
+    $this->click("//input[@value='Search']");
+
+    $this->assertTextPresent($airport['name']);
+    $this->click("//input[@value='Edit']");
+
+    $this->assertValue("airport", $airport['name']);
+    $this->assertNotEditable("//input[@value='Save changes']");
+
+    $this->focus("airport");
+    $this->type("airport", "EditedName");
     $this->assertEditable("//input[@value='Save changes']");
+
+    $this->click("//input[@value='Save changes']");
+
+    $this->selectWindow();
+    $this->assertValue("src_ap1", "Testville-EditedName");
   }
 }
 ?>

test/client/OpenFlightsSeleniumTestCase.php
@@ -7,7 +7,7 @@ class OpenFlightsSeleniumTestCase extends PHPUnit_Extensions_SeleniumTestCase
 {
   protected $captureScreenshotOnFailure = TRUE;
   protected $screenshotPath = '/tmp/screenshots';
-  protected $screenshotUrl = 'http://localhost/screenshots';
+  protected $screenshotUrl = '/tmp/screenshots';
 
   protected function setUp()
   {

test/client/WebLoginTest.php
@@ -40,6 +40,27 @@ class WebLoginTest extends OpenFlightsSeleniumTestCase
     $this->verifyTextPresent("Hi, ${settings['name']} !");
     $this->verifyTextPresent('1 segments');
     $this->verifyTextPresent('1000 miles');
+
+    $this->click("//input[@value='New flight']");
+    $this->verifyTextPresent("Add new flights");
+  }
+
+  public function testReload()
+  {
+    global $settings;
+
+    $this->open('/');
+    $this->type('name', $settings['name']);
+    $this->type('pw', $settings['password']);
+    $this->click('loginbutton');
+    sleep(1);
+    $this->verifyTextPresent("Hi, ${settings['name']} !");
+
+    $this->open('/');
+    $this->verifyTextPresent("Logged in as ${settings['name']}");
+
+    $this->click("//input[@value='New flight']");
+    $this->verifyTextPresent("Add new flights");
   }
 }
 ?>

test/client/run-suite.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 SELENIUM_SERVER=../selenium/selenium-server-standalone-2.0rc2.jar
 
+echo Setting up screenshot directory...
+mkdir -p /tmp/screenshots
+rm /tmp/screenshots/*
+
 echo Bootstrapping...
-php ../server/cleanup.php >>log/bootstrap.log
+php ../server/cleanup.php >log/bootstrap.log
 php ../server/settings.php >log/bootstrap.log
 php ../server/submit.php >>log/bootstrap.log
+php ../server/apsearch.php >>log/bootstrap.log
 
 if (ps -ef | grep -q "selenium-server.*jar$"); then
   echo Selenium Server already running:

commit 49ff51201141660694e4cd8da711cc0ddad749d1

apsearch: Fix "Results NaN to NaN of"
Probably broken since 4c91eeda44d3747955abfd21170c2f12784d004a

Fixes https://github.com/jpatokal/openflights/issues/1348

js/apsearch.js
@@ -395,8 +395,8 @@ function searchResult(str) {
     warning = null;
   }
 
-  var max = json["max"];
-  if (max == 0) {
+  if (json.hasOwnProperty("status") && json["status"] === 0) {
+    // TODO: We're ignoring json["message"]... Do we care?
     table +=
       "<tr><td><i>" +
       gt.gettext("No matches found in this database.") +