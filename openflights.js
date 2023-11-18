jpatokal/openflights

commit 20743995a4881747973fd6a097e5b79bbc769d21

Replace hinttextbox with html placeholder attribute
Fixes https://github.com/jpatokal/openflights/issues/1257

openflights.js
@@ -409,8 +409,6 @@ function init() {
       query = arguments[1];
   }
 
-  initHintTextboxes();
-
   prepareAutocomplete("qs", {
     successCb: getQuickSearchId,
     failureCb: (e) => undefined,
@@ -1026,8 +1024,6 @@ function xmlhttpPost(strURL, id, param) {
           } else {
             updateFilter(str);
             closePopup(true);
-            $("qs").value = $("qs").hintText;
-            $("qs").style.color = "#888";
             $("qsid").value = 0;
             $("qsgo").disabled = true;
             if (filter_alid == 0) {
@@ -1164,7 +1160,8 @@ function xmlhttpPost(strURL, id, param) {
           var alid = $("airline" + indexes[i] + "id").value;
           var airline = $("airline" + indexes[i]).value.trim();
           if (!alid || alid == 0) {
-            if (airline == "" || airline == $("airline").hintText) {
+            // TODO: Do we need this comparison against placeholder?
+            if (airline == "" || airline == $("airline").placeholder) {
               alid = "-1"; // UNKNOWN
             } else {
               mode = getMode();
@@ -2658,10 +2655,7 @@ function editFlight(str, param) {
   form.registration.value = col[14];
   alid = col[15];
   if (col[16] != "") {
-    $("note").style.color = "#000";
     form.note.value = col[16];
-  } else {
-    $("note").value = $("note").hintText;
   }
   form.seat.value = col[8];
 
@@ -2974,7 +2968,8 @@ function airportCodeToAirport(type) {
 
 // User has entered invalid input: clear apid, turn field red (unless empty) and remove marker
 function invalidateField(type, airport = false) {
-  if ($(type).value != "" && $(type).value != $(type).hintText) {
+  // TODO: Do we need this comparison against placeholder?
+  if ($(type).value != "" && $(type).value != $(type).placeholder) {
     $(type).style.color = "#FF0000";
   }
   $(type + "id").value = 0;
@@ -3363,7 +3358,8 @@ function markAirport(element, quick) {
       lineLayer.addFeatures(input_line);
       oldDist = $("distance").value;
       $("distance").value = distance;
-      if (oldDist == "" && $("dst_time").value != $("dst_time").hintText) {
+      // TODO: Do we need this comparison against placeholder?
+      if (oldDist == "" && $("dst_time").value != $("dst_time").placeholder) {
         // user has already manually entered arrival time
         calcDuration("ARRIVAL");
       } else {
@@ -4000,11 +3996,9 @@ function closeInput() {
 
 // Clear out (restore to defaults) the time indicators in the editor
 function clearTimes() {
-  $("src_time").value = $("src_time").hintText;
-  $("dst_time").value = $("dst_time").hintText;
+  $("src_time").value = "";
+  $("dst_time").value = "";
   $("dst_days").value = "";
-  $("src_time").style.color = "#888";
-  $("dst_time").style.color = "#888";
   $("dst_days").style.display = "none";
 }
 
@@ -4020,7 +4014,6 @@ function todayString() {
 
 // Clear out (restore to defaults) the input box
 function clearInput() {
-  resetHintTextboxes();
   if (getCurrentPane() == "input") {
     var form = document.forms["inputform"];
     form.src_date.value = todayString();

index.php
@@ -198,8 +198,8 @@ require_once "./php/helper.php";
   <td><font color="blue"><?php echo _("Date") ?></font></td>
   <td colspan=2>
     <input type="text" name="src_date" id="src_date" size="11" class="date" tabindex="11" onChange="JavaScript:calcDuration('DEPARTURE');"/> <img src="/img/scw.gif" height="15" width="16" onclick="scwShow(scwID('src_date'),event); calcDuration('DEPARTURE');" />
-    <input type="text" name="src_time" id="src_time" tabindex="12" size="5" class="hintTextboxMini" value="HH:MM" onChange="JavaScript:calcDuration('DEPARTURE')"/> &rarr;
-    <input type="text" name="dst_time" id="dst_time" size="5" class="hintTextboxMini" value="HH:MM" onChange="JavaScript:calcDuration('ARRIVAL')"/>
+    <input type="text" name="src_time" id="src_time" tabindex="12" size="5" class="hint2TextboxMini" placeholder="HH:MM" onChange="JavaScript:calcDuration('DEPARTURE')"/> &rarr;
+    <input type="text" name="dst_time" id="dst_time" size="5" class="hint2TextboxMini" placeholder="HH:MM" onChange="JavaScript:calcDuration('ARRIVAL')"/>
     <input type="text" name="dst_days" id="dst_days" size="6" value="" style="border: 0px; display: none" onChange="JavaScript:markAsChanged()"/><img src="/img/icon_clock.png" id="icon_clock" title="<?php echo _("Departure and arrival time") ?>" height="16" width="16" onclick='JavaScript:help("time")'/>
   </td>
 

js/utilities.js
@@ -1,62 +1,4 @@
-// Third-party utility code for hinted textboxes, sortable tables
-
-// Script by Drew Noakes
-// http://drewnoakes.com
-// 14 Dec 2006 - Initial release
-// 08 Jun 2010 - Added support for password textboxes
-
-var HintClass = "hintTextbox";
-var HintActiveClass = "hintTextboxActive";
-
-var MiniHintClass = "hintTextboxMini";
-var MiniHintActiveClass = "hintTextboxActiveMini";
-
-function initHintTextboxes() {
-  var inputs = document.getElementsByTagName('input');
-  for (i=0; i<inputs.length; i++) {
-    var input = inputs[i];
-    if (input.type!="text" && input.type!="password")
-      continue;
-
-    if (input.className.indexOf(HintClass)!=-1) {
-      input.hintText = input.value;
-      input.className = input.className.indexOf(MiniHintClass)!=-1 ? MiniHintClass : HintClass;
-      input.onfocus = onHintTextboxFocus;
-      input.onblur = onHintTextboxBlur;
-    }
-  }
-}
-
-function resetHintTextboxes() {
-  var inputs = document.getElementsByTagName('input');
-  for (i=0; i<inputs.length; i++) {
-    var input = inputs[i];
-    if (input.type!="text" && input.type!="password")
-      continue;
-
-    // Matches active and non-active
-    if (input.className.indexOf(HintClass)!=-1) {
-      input.className = input.className.indexOf(MiniHintClass)!=-1 ? MiniHintClass : HintClass;
-      input.value = input.hintText;
-    }
-  }
-}
-
-function onHintTextboxFocus() {
-  var input = this;
-  if (input.value.trim()==input.hintText) {
-    input.value = "";
-    input.className = input.className.indexOf(MiniHintClass)!=-1 ? MiniHintActiveClass : HintActiveClass;
-  }
-}
-
-function onHintTextboxBlur() {
-  var input = this;
-  if (input.value.trim().length==0) {
-    input.value = input.hintText;
-    input.className = input.className.indexOf(MiniHintActiveClass)!=-1 ? MiniHintClass : HintClass;
-  }
-}
+// Third-party utility code for sortable tables
 
 /*
 Table sorting script by Joost de Valk, check it out at http://www.joostdevalk.nl/code/sortable-table/.

openflights.css
@@ -225,8 +225,6 @@ input.hint2Textbox {
   max-width: 350px;
 }
 
-input.hintTextboxMini,
-input.hintTextboxActiveMini,
 input.hint2TextboxMini {
   min-width: 60px;
   max-width: 60px;

commit b4a9ae3ccdf85692a636dc30e6d0f95bea07bbee

openflights.js: Unconditionally set notes field
Caused by 20743995a4881747973fd6a097e5b79bbc769d21

Fixes https://github.com/jpatokal/openflights/issues/1452

openflights.js
@@ -2925,9 +2925,7 @@ function editFlight(str, param) {
 
   form.registration.value = col[14];
   alid = col[15];
-  if (col[16] != "") {
-    form.note.value = col[16];
-  }
+  form.note.value = col[16];
   form.seat.value = col[8];
 
   $("src_ap").style.color = "#000000";